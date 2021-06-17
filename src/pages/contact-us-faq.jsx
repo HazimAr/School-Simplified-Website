0083a2d7bc9e6b86419fa2da96f825d4d95cae16
Devin <lightningowl2004@gmail.com>
2021-06-16 22:35:52 -0700

    Update contact-us-faq.jsx

---
+++ src/pages/contact-us-faq.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    render() {
+        return (
+            <form action="">
+                <div>
+                    <label>Username</label>
+                    <input type="text" />
+                </div>
+            </form>
+        )
+    }
+}
+
+export default Form

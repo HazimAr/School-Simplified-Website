93f08ff5f3f1f119d4e071d37a8bce6922658346
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-18 14:44:41 -0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,16 @@ import { Flex, Image, Link } from "@chakra-ui/react";
 // eslint-disable-next-line import/no-default-export
 export default function Main(): JSX.Element {
 	return (
-		<Flex bg="#5a60ad" py="20px" justify="center" align="center">
+		<Flex bg="#5a60ae" justify="center" align="center">
 			<Flex
+				justify="space-between"
 				maxW="1200px"
 				w="100%"
-				as="header"
-				justify="space-between"
-				align="center"
 				mx="50px"
+				py="10px"
+				textAlign="left"
+				align="center"
+				flexWrap="wrap"
 			>
 				<Image src="/logo.png" w="200px" />
 				<Flex>

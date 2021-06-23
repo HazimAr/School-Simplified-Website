4a769de898e4fae4c8459967524199cad73aba31
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-22 20:56:28 -0700

    Fixed Donate

---
+++ src/pages/donate.tsx
@@ -0,0 +1,17 @@
+import { Text, Box } from "@chakra-ui/react";
+import Container from "@components/container";
+
+export default function Donate(): JSX.Element {
+	return (
+		<Container>
+			<Box>
+				<Text>Hello, World!</Text>
+				<Text>Hello, World!</Text>
+				<Text>Hello, World!</Text>
+				<Text>Hello, World!</Text>
+				<Text>Hello, World!</Text>
+				<Text>Hello, World!</Text>
+			</Box>
+		</Container>
+	);
+}

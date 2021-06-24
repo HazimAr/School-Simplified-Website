7ccb16c98d5f890c0bfa55331876cc7721eb672f
yasmeene <yasmeenelkheir@gmail.com>
2021-06-23 19:48:47 -0500

    Update community.tsx

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -7,32 +7,41 @@ import {
 	Flex,
 	Spacer,
 	Divider,
+    Container,
+    Grid,
+    GridItem,
+    Image,
+
 } from "@chakra-ui/react";
 
 export default function Home(): JSX.Element {
 	return (
 		<Box>
+            <Container bg="brand.purple.dark" maxW='xlg'>
+                <Text fontSize='6xl'>X</Text>
+            </Container>
 			{/* art box */}
 			<Flex textAlign="left">
-				<Wrap spacing="40px" justify="left" margin="25px">
+				<Wrap spacing="40px" justify="left" margin="20px">
 					<VStack spacing={4}>
 						<Heading size="md">Our Community</Heading>
-
-						<Text noOfLines={6}>
-							{" "}
-							Our organization features a global community of over
-							60,000 teenagers! Diverse, open-minded, and
-							inclusive, our students enjoy conversing in an
-							equitable environment where every new acquaintance
-							becomes a longtime friend!
-						</Text>
-
+                        <Container maxW='container.sm'>
+                            <Text>
+                                {" "}
+                                Our organization features a global community of over
+                                60,000 teenagers! Diverse, open-minded, and
+                                inclusive, our students enjoy conversing in an
+                                equitable environment where every new acquaintance
+                                becomes a longtime friend!
+                            </Text>
+                        </Container>
+                    
 						<Box
 							w="43vh"
 							h="48vh"
 							backgroundColor="#e0d5ff"
 							padding="5px"
-							rounded="40px"
+							rounded="50px"
 						>
 							{/* community art goes here */}
 						</Box>
@@ -46,7 +55,7 @@ export default function Home(): JSX.Element {
 
 				<Spacer />
 
-				<Wrap margin="15px" spacing="40px">
+				<Wrap margin="30px" spacing="40px">
 					<VStack spacing={6}>
 						{/* info boxes */}
 						<Box
@@ -100,11 +109,19 @@ export default function Home(): JSX.Element {
 						w="43vh"
 						h="48vh"
 						backgroundColor="#d8b2fb"
-						padding="5px"
-						rounded="40px"
+						padding="15px"
+						rounded="50px"
 					>
+
 						{/* photo goes here */}
-						<Text>
+                        <Image
+                            boxSize="25vh"
+                            objectFit="cover"
+                            src=""
+                            alt="Discord"
+                        />
+
+						<Text fontSize='3vh'>
 							{" "}
 							Join our Discord. Come for the academic help, stay
 							for the events, live music, and friends!{" "}
@@ -126,15 +143,38 @@ export default function Home(): JSX.Element {
 			</Flex>
 
 			<Divider orientation="horizontal" borderColor="white" />
-			<Flex textAlign="left">
-				<Wrap spacing="30px" margin="20px">
 					{/* staff box */}
-					<Box>
-						<Heading mb="5vh"> Our Staff </Heading>
-						<Text>School Simplified staff</Text>
-					</Box>
-				</Wrap>
-			</Flex>
+			        <Grid templateColumns="repeat(5, 1fr)" gap={5} margin='15px'>
+                        <GridItem colSpan={1} h="10" mb='8vh'>
+                            <Heading mb="3vh"> Our Staff </Heading>
+
+                            <Text>School Simplified staff </Text>
+                        </GridItem>
+
+                        <GridItem colStart={4} colEnd={6} h="10">
+                            <Heading as='h1' size='4xl'> X </Heading>
+                        </GridItem>
+                    </Grid>
+
+            <Divider orientation="horizontal" borderColor="white" />
+            <Flex textAlign="left">
+				<Wrap spacing="40px" justify="left" margin="20px">
+                    <VStack spacing={4}>
+                        <Heading size="md">Our Socials</Heading>
+                            <Container maxW='container.sm'>
+                                <Text fontSize='lg'>
+                                    {" "}
+                                    Our organization features a global community of over
+                                    60,000 teenagers! Diverse, open-minded, and
+                                    inclusive, our students enjoy conversing in an
+                                    equitable environment where every new acquaintance
+                                    becomes a longtime friend!
+                                </Text>
+                            </Container>
+                    </VStack>
+                </Wrap>
+            </Flex>
+
 		</Box>
 	);
 }

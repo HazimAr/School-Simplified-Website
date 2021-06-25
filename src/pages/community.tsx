ae57e4f580551cf4861f08f52fd5046b676901cc
yasmeene <yasmeenelkheir@gmail.com>
2021-06-25 15:57:07 -0500

    added disc logo + link to disc

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -11,6 +11,8 @@ import {
     Grid,
     GridItem,
     Image,
+	Center,
+	Link,
 
 } from "@chakra-ui/react";
 
@@ -26,14 +28,14 @@ export default function Home(): JSX.Element {
 					<VStack spacing={4}>
 						<Heading size="md">Our Community</Heading>
                         <Container maxW='container.sm'>
-                            <Text>
+                            <Heading  size='sm'>
                                 {" "}
                                 Our organization features a global community of over
                                 60,000 teenagers! Diverse, open-minded, and
                                 inclusive, our students enjoy conversing in an
                                 equitable environment where every new acquaintance
                                 becomes a longtime friend!
-                            </Text>
+                            </Heading>
                         </Container>
                     
 						<Box
@@ -42,6 +44,7 @@ export default function Home(): JSX.Element {
 							backgroundColor="#e0d5ff"
 							padding="5px"
 							rounded="50px"
+							
 						>
 							{/* community art goes here */}
 						</Box>
@@ -102,26 +105,29 @@ export default function Home(): JSX.Element {
 
 			<Divider orientation="horizontal" borderColor="white" />
 
-			<Flex textAlign="left">
+			<Flex textAlign="left" >
 				<Wrap spacing="40px" justify="left" margin="25px">
 					{/* disc box */}
 					<Box
 						w="43vh"
 						h="48vh"
-						backgroundColor="#d8b2fb"
-						padding="15px"
+						backgroundColor="#FFFFFF"
+						padding="13px"
 						rounded="50px"
+						opacity ='48%'
 					>
+						<Center>
+							<Link href='https://discord.com/invite/ghUAx6AQU4' isExternal>
+								<Image
+									src="./discord.png"
+									boxSize='215px'
+									objectFit="cover"
+									alt="Discord"
+								/>
+							</Link> 
+						</Center>
 
-						{/* photo goes here */}
-                        <Image
-                            boxSize="25vh"
-                            objectFit="cover"
-                            src=""
-                            alt="Discord"
-                        />
-
-						<Text fontSize='3vh'>
+						<Text fontSize='2.9vh' color='#5A60AD' >
 							{" "}
 							Join our Discord. Come for the academic help, stay
 							for the events, live music, and friends!{" "}
@@ -133,11 +139,11 @@ export default function Home(): JSX.Element {
 				<Wrap spacing="30px" margin="20px">
 					{/* members box */}
 					<Box>
-						<Heading mb="8vh"> Our Members </Heading>
-						<Text>
+						<Heading mb="5vh"> Our Members </Heading>
+						<Heading size='sm'>
 							Our community consists of members ranging from
 							teenagers to teachers alike.{" "}
-						</Text>
+						</Heading>
 					</Box>
 				</Wrap>
 			</Flex>
@@ -148,7 +154,7 @@ export default function Home(): JSX.Element {
                         <GridItem colSpan={1} h="10" mb='8vh'>
                             <Heading mb="3vh"> Our Staff </Heading>
 
-                            <Text>School Simplified staff </Text>
+                            <Heading size='sm'>School Simplified staff </Heading>
                         </GridItem>
 
                         <GridItem colStart={4} colEnd={6} h="10">
@@ -160,16 +166,16 @@ export default function Home(): JSX.Element {
             <Flex textAlign="left">
 				<Wrap spacing="40px" justify="left" margin="20px">
                     <VStack spacing={4}>
-                        <Heading size="md">Our Socials</Heading>
+                        <Heading >Our Socials</Heading>
                             <Container maxW='container.sm'>
-                                <Text fontSize='lg'>
+                                <Heading size='sm'>
                                     {" "}
                                     Our organization features a global community of over
                                     60,000 teenagers! Diverse, open-minded, and
                                     inclusive, our students enjoy conversing in an
                                     equitable environment where every new acquaintance
                                     becomes a longtime friend!
-                                </Text>
+                                </Heading>
                             </Container>
                     </VStack>
                 </Wrap>

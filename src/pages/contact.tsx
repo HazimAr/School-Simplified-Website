0c3702c1fc5bded16479d64264b817e914562195
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-26 18:59:09 -0700

    Container/ Container inside

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,51 +1,67 @@
-import { Heading, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image } from "@chakra-ui/react";
 import ContactForm from "@components/contact-form";
-import FaqList from "@components/faq-list";
 import Container from "@components/container";
+import ContainerInside from "@components/containerInside";
+import FaqList from "@components/faq-list";
+import React from "react";
 
 export default function Contact(): JSX.Element {
 	const qaPairs = [
 		{
-			"question": "Who are you?",
-			"answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+			question: "Who are you?",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 		},
 		{
-			"question": "Who started School Simplified?",
-			"answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+			question: "Who started School Simplified?",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 		},
 		{
-			"question": "Is School Simplified free to use?",
-			"answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+			question: "Is School Simplified free to use?",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 		},
 		{
-			"question": "How can I get community service hours?",
-			"answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+			question: "How can I get community service hours?",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 		},
 		{
-			"question": "How reliable is School Simplified?",
-			"answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+			question: "How reliable is School Simplified?",
+			answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 		},
-	]
+	];
 
 	return (
 		<>
-			<Container 
-				flexDirection={{ sm: "column", lg: "row" }} 
-				justify="space-between"
-				px={{ sm: 10, md: 100, lg: 200}}
-			>
-				<Container flexDirection="column" flex={1} py={10}>
-					<Heading as="h1" size="xl" mb={3}>Get in Touch</Heading>
-					<Heading as="h2" size="md" mb={3}>Have a Question? Comment? Concern?</Heading>
-					<Image src="/contactPerson.png" w="200px" />
-				</Container>
-				<Container flex={2} py={10}>
-					<ContactForm />
-				</Container>
+			<Container>
+				<ContainerInside>
+					<Flex
+						flexDirection={{ sm: "column", lg: "row" }}
+						justify="space-between"
+						px={{ sm: 10, md: 100, lg: 200 }}
+					>
+						<Box flexDirection="column" flex={1} py={10}>
+							<Heading as="h1" size="xl" mb={3}>
+								Get in Touch
+							</Heading>
+							<Heading as="h2" size="md" mb={3}>
+								Have a Question? Comment? Concern?
+							</Heading>
+							<Image src="/contactPerson.png" w="200px" />
+						</Box>
+						<Box flex={2} py={10}>
+							<ContactForm />
+						</Box>
+					</Flex>
+				</ContainerInside>
 			</Container>
-			<Container flexDirection="column" align="stretch" >
-				<Heading as="h1" size="xl" mb={3}>FAQ</Heading>
-				<FaqList list={qaPairs} />
+			<Container>
+				<ContainerInside>
+					<Flex flexDirection="column" align="stretch">
+						<Heading as="h1" size="xl" mb={3}>
+							FAQ
+						</Heading>
+						<FaqList list={qaPairs} />
+					</Flex>
+				</ContainerInside>
 			</Container>
 		</>
 	);

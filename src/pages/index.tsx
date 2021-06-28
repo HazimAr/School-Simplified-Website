0d47556d2f5ba1ec0f5014758b427cc65834ce4a
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-27 20:58:35 -0700

    Redsign home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,31 @@ import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 
+const spacing = 20;
+
 export default function Home(): JSX.Element {
 	return (
 		<Container>
 			<ContainerInside>
-				<Flex justify="space-between" align="center">
-					<Box maxW="400px" textAlign="left" py="100px">
+				<Flex
+					justify="center"
+					align="center"
+					py="100px"
+					flexWrap="wrap"
+				>
+					<Box maxW="50ch" textAlign="left" my={`${spacing}px`}>
 						<Heading>The key to your future</Heading>
-						<Text>
+						<Text my="10px">
+							Lorem ipsum dolor sit amet consectetur, adipisicing
+							elit. Culpa, ducimus inventore? Maiores incidunt
+							possimus corrupti provident modi consequuntur
+						</Text>
+						<Button>Learn More</Button>
+					</Box>
+					{/* <Spacer /> */}
+					<Box maxW="50ch" textAlign="left" my={`${spacing}px`}>
+						<Heading>The key to your future</Heading>
+						<Text my="10px">
 							Lorem ipsum dolor sit amet consectetur, adipisicing
 							elit. Culpa, ducimus inventore? Maiores incidunt
 							possimus corrupti provident modi consequuntur

abad95ce553d821ed6510665e468f793386f5092
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-27 20:29:24 -0700

    button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 
@@ -6,31 +7,16 @@ export default function Home(): JSX.Element {
 	return (
 		<Container>
 			<ContainerInside>
-				<Flex padding="90px">
-					<Container w="1200px" maxW="1000px">
-						<Flex
-							w="100%"
-							h="400px"
-							bg="brand.transparent"
-							justify="space-between"
-							align="center"
-						>
-							<Box>
-								<Heading size="lg">
-									The key to your
-									<br />
-									success.
-								</Heading>
-								<Text fontSize="lg" mt="auto">
-									A non-profit here to help you with all your
-									academic and extracurricular needs.
-									Everything you need is simply one click
-									away.
-								</Text>
-							</Box>
-							<Image src="./key.png" />
-						</Flex>
-					</Container>
+				<Flex justify="space-between" align="center">
+					<Box maxW="400px" textAlign="left" py="100px">
+						<Heading>The key to your future</Heading>
+						<Text>
+							Lorem ipsum dolor sit amet consectetur, adipisicing
+							elit. Culpa, ducimus inventore? Maiores incidunt
+							possimus corrupti provident modi consequuntur
+						</Text>
+						<Button>Learn More</Button>
+					</Box>
 				</Flex>
 			</ContainerInside>
 		</Container>

e6455afdf05f5feedbcaeefde9d9f8e1d03c19aa
Gui Guy <jedandeganwang1999@gmail.com>
2021-07-05 22:27:08 -0500

    TypeScript is pain

--- src/components/volunteering/rotating_panel.tsx
+++ src/components/volunteering/rotating_panel.tsx
@@ -1,18 +1,43 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image, Heading, Text } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 import React, { ReactNode } from "react";
 
-export default class RotatingPanel extends React.Component {
-	// constructor(props: any) {
-	// 	super(props);
-	// }
+const teams: PanelProps[] = [
+	{
+		src: "https://picsum.photos/300/300",
+		teamName: "Example Team",
+		teamDesc: "alskjdfalkdsfj",
+	},
+];
+
+interface State {
+	innerPanels: JSX.Element[];
+	index: number;
+}
+
+export default class RotatingPanel extends React.Component<any, State> {
+	constructor(props: any) {
+		super(props);
+
+		const innerPanels = teams.map((v, idx: number) => {
+			return (
+				<Panel
+					src={v.src}
+					teamName={v.teamName}
+					teamDesc={v.teamDesc}
+					key={"item_" + idx}
+				/>
+			);
+		});
+		this.state = { innerPanels: innerPanels, index: 0 };
+	}
 
 	render(): ReactNode {
 		return (
-			<Container bg="brand.transparent2">
+			<Container bg="brand.transparent2" {...this.props}>
 				<ContainerInside py={8}>
-					<Panel src="aslkjalsdkfj" />
+					{this.state.innerPanels[this.state.index]}
 				</ContainerInside>
 			</Container>
 		);
@@ -22,6 +47,8 @@ export default class RotatingPanel extends React.Component {
 interface PanelProps {
 	children?: any;
 	src: string;
+	teamName: string;
+	teamDesc: string;
 }
 
 class Panel extends React.Component<PanelProps, any> {
@@ -31,8 +58,10 @@ class Panel extends React.Component<PanelProps, any> {
 
 	render(): ReactNode {
 		return (
-			<Box>
+			<Box {...this.props}>
 				<Image src={this.props.src} />
+				<Heading size="lg">{this.props.teamName}</Heading>
+				<Text>{this.props.teamDesc}</Text>
 			</Box>
 		);
 	}

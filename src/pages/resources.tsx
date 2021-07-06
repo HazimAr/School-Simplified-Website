a199dcc7b004cfa637c73593a0a38868723c1883
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-06 14:21:10 -0700

    Update resources.tsx

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,35 +1,27 @@
-import {
-	Box,
-	Button,
-	Heading,
-	Menu,
-	MenuButton,
-	MenuItem,
-	MenuList,
-	Text,
-	Flex, 
-	Spacer
-} from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 
 export default function Resources(): JSX.Element {
 	return (
-		<Flex>
-			<Box pl="20px" flex="0.75" bg="brand.purple.dark">
-				<Heading align="left">NOTES</Heading>
-				<Text align="left" >We simplify complicated material by offering 
-					notes and free help with classes! Our resources 
-					cover both AP and non-AP courses, allowing you 
-					to explore the intricacies of the subject at hand.</Text>
-			</Box>
-			<Box flex="2" bg="brand.purple.dark">
-			</Box>
-
-
-		</Flex>
-
-		
-		
+		<Container>
+			<ContainerInside>
+				<Flex>
+					<Box pl="20px" flex="0.75" bg="brand.purple.dark">
+						<Heading align="left">NOTES</Heading>
+						<Text align="left">
+							We simplify complicated material by offering notes
+							and free help with classes! Our resources cover both
+							AP and non-AP courses, allowing you to explore the
+							intricacies of the subject at hand.
+						</Text>
+					</Box>
+					<Box flex="2" bg="brand.purple.dark">
+					
+						hehe img here
+					</Box>
+				</Flex>
+			</ContainerInside>
+		</Container>
 	);
 }

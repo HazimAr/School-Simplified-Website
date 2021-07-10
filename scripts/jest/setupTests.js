764194752b7d2c4ef00089633ab7e9f9cd6161fb
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-09 18:32:45 -0700

    google api

--- scripts/jest/setupTests.js
+++
@@ -1,9 +0,0 @@
-afterEach(() => {
-	window.localStorage.clear();
-
-	// Object.keys(parse(document.cookie)).forEach((key) => {
-	// 	document.cookie = serialize(key, "", {
-	// 		maxAge: -1,
-	// 	});
-	// });
-});

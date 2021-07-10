1ba95b9fe5bbb0f5fe3f3ca9981f55719a940186
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-09 22:39:13 -0700

    Update notion.ts

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -20,53 +20,75 @@ async function getSubjects() {
 		);
 	});
 	let subjects;
-	await Promise.all(promises).then((axiosClassPromises) => {
-		subjects = data.results.map((page: any, index: number) => {
-			const currentSubject = axiosClassPromises[index].data.results;
-			const content = currentSubject.map(
-				async (currentClass, index: number) => {
-					const promises2 = currentSubject.map((page: any) => {
-						return axios.get(
-							`https://api.notion.com/v1/blocks/${currentClass.id}/children`,
-							config
-						);
-					});
-					const allPromises = await Promise.all(promises2);
-					const blocks = allPromises[index].data.results;
-					const content = blocks
-						.filter(
-							(block) =>
-								block.type === "heading_3" &&
-								block.heading_3.text.length > 0
+
+	await Promise.all(promises).then(async (axiosClassPromises) => {
+		let subjectsInside;
+		await Promise.all(
+			(subjects = data.results.map(
+				async (page: any, pageIndex: number) => {
+					const currentSubject =
+						axiosClassPromises[pageIndex].data.results;
+					let content;
+					await Promise.all(
+						currentSubject.map(
+							async (currentClass, classIndex: number) => {
+								const promises2 = currentSubject.map(
+									(page: any) => {
+										return axios.get(
+											`https://api.notion.com/v1/blocks/${currentClass.id}/children`,
+											config
+										);
+									}
+								);
+								const allPromises = await Promise.all(
+									promises2
+								);
+								const blocks =
+									allPromises[classIndex].data.results;
+								const content = blocks
+									.filter(
+										(block) =>
+											block.type === "heading_3" &&
+											block.heading_3.text.length > 0
+									)
+									.map((block) => {
+										// console.log(block);
+										return {
+											title:
+												block.heading_3.text[0]
+													?.plain_text ??
+												"I was broken :(",
+											content: [
+												{
+													title: "Interesting thing #1",
+													href: "/s",
+												},
+											],
+										};
+									});
+								if (pageIndex === 0 && classIndex === 0) {
+									console.log(
+										"THIS IS THE CONTENT BITCH " +
+											JSON.stringify(content) +
+											"\n"
+									);
+								}
+								return {
+									title: currentClass?.child_page?.title,
+									content: content,
+								};
+							}
 						)
-						.map((block) => {
-							// console.log(block);
-							return {
-								title:
-									block.heading_3.text[0]?.plain_text ??
-									"I was broken :(",
-								content: [
-									{
-										title: "Interesting thing #1",
-										href: "/s",
-									},
-								],
-							};
-						});
-					console.log(
-						"THIS IS THE CONTENT BITCH " + JSON.stringify(content)
-					);
+					).then((content) => (content = content));
 					return {
-						title: currentClass?.child_page?.title,
+						title: page.properties.Name.title[0].plain_text,
+						id: page.id,
 						content: content,
 					};
 				}
-			);
-			return {
-				title: page.properties.Name.title[0].plain_text,
-				id: page.id,
-				content: content,
-			};
+			))
+		).then((subject) => {
+			subjectsInside = subject;
 		});
 	});
 	return subjects;

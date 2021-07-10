2eda1a245c1ad636df20fd8397dd9b49323b13b7
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-09 20:53:50 -0700

    Update notion.ts

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -1,63 +1,76 @@
 import axios from "axios";
 
+const config = {
+	headers: {
+		Authorization: "Bearer " + process.env.NOTION_API_KEY ?? "",
+		"Notion-Version": "2021-05-13",
+	},
+};
+
 async function getSubjects() {
 	const { data } = await axios.post(
 		`https://api.notion.com/v1/databases/283ca488c1624a4fbef37f1d8bd8da90/query`,
 		{},
-		{
-			headers: {
-				Authorization: "Bearer " + process.env.NOTION_API_KEY ?? "",
-				"Notion-Version": "2021-05-13",
-			},
-		}
+		config
 	);
-	const subjects = data.results.map((page) => {
-		return {
-			title: page.properties.Name.title[0].plain_text,
-			id: page.id,
-			content: [
-				{
-					title: "Algebra I",
-					content: [
-						{
-							title: "Quadratics",
-							content: [
-								{
-									title: "Interesting thing #1",
-									href: "/s",
-								},
-								{
-									title: "Interesting thing #2",
-									href: "/a",
-								},
-								{
-									title: "Interesting thing #3",
-									href: "/a",
-								},
-								{
-									title: "Interesting thing #4",
-									href: "/f",
-								},
-								{
-									title: "Interesting thing #5",
-									href: "/e",
-								},
-								{
-									title: "Interesting thing #6",
-									href: "/v",
-								},
-								{
-									title: "Interesting thing #7",
-									href: "/w",
-								},
-							],
-						},
-					],
-				},
-			],
-		};
+	const promises = data.results.map((page: any) => {
+		return axios.get(
+			`https://api.notion.com/v1/blocks/${page.id}/children`,
+			config
+		);
 	});
-	return subjects;
+	Promise.all(promises).then((classes) => {
+		const subjects = data.results.map((page: any, index: number) => {
+			return {
+				title: page.properties.Name.title[0].plain_text,
+				id: page.id,
+				content: [
+					{
+						title: classes[index].data.results[0]?.child_page
+							?.title,
+						content: [
+							{
+								title: "Quadratics",
+								content: [
+									{
+										title: "Interesting thing #1",
+										href: "/s",
+									},
+									{
+										title: "Interesting thing #2",
+										href: "/a",
+									},
+									{
+										title: "Interesting thing #3",
+										href: "/a",
+									},
+									{
+										title: "Interesting thing #4",
+										href: "/f",
+									},
+									{
+										title: "Interesting thing #5",
+										href: "/e",
+									},
+									{
+										title: "Interesting thing #6",
+										href: "/v",
+									},
+									{
+										title: "Interesting thing #7",
+										href: "/w",
+									},
+								],
+							},
+						],
+					},
+				],
+			};
+		});
+		console.log(subjects[0]);
+		return subjects;
+	});
+	return 1;
 }
 
 export { getSubjects };

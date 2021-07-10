5eb9f7b0bb5fa0f4a9ace407790b53a5b295dd19
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-09 21:08:26 -0700

    Update notion.ts

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -19,58 +19,59 @@ async function getSubjects() {
 			config
 		);
 	});
-	Promise.all(promises).then((classes) => {
-		const subjects = data.results.map((page: any, index: number) => {
+	let subjects;
+	await Promise.all(promises).then((classes) => {
+		subjects = data.results.map((page: any) => {
+			const content = classes.map((myClass, index: number) => {
+				return {
+					title: myClass.data.results[0]?.child_page?.title,
+					content: [
+						{
+							title: "Quadratics",
+							content: [
+								{
+									title: "Interesting thing #1",
+									href: "/s",
+								},
+								{
+									title: "Interesting thing #2",
+									href: "/a",
+								},
+								{
+									title: "Interesting thing #3",
+									href: "/a",
+								},
+								{
+									title: "Interesting thing #4",
+									href: "/f",
+								},
+								{
+									title: "Interesting thing #5",
+									href: "/e",
+								},
+								{
+									title: "Interesting thing #6",
+									href: "/v",
+								},
+								{
+									title: "Interesting thing #7",
+									href: "/w",
+								},
+							],
+						},
+					],
+				};
+			});
 			return {
 				title: page.properties.Name.title[0].plain_text,
 				id: page.id,
-				content: [
-					{
-						title: classes[index].data.results[0]?.child_page
-							?.title,
-						content: [
-							{
-								title: "Quadratics",
-								content: [
-									{
-										title: "Interesting thing #1",
-										href: "/s",
-									},
-									{
-										title: "Interesting thing #2",
-										href: "/a",
-									},
-									{
-										title: "Interesting thing #3",
-										href: "/a",
-									},
-									{
-										title: "Interesting thing #4",
-										href: "/f",
-									},
-									{
-										title: "Interesting thing #5",
-										href: "/e",
-									},
-									{
-										title: "Interesting thing #6",
-										href: "/v",
-									},
-									{
-										title: "Interesting thing #7",
-										href: "/w",
-									},
-								],
-							},
-						],
-					},
-				],
+				content: content,
 			};
 		});
 		console.log(subjects[0]);
-		return subjects;
 	});
-	return 1;
+	return subjects;
 }
 
 export { getSubjects };
+

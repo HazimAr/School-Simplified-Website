c52053839139864a308736cb00517aa237222114
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-13 21:50:10 -0700

    Update community.tsx

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -1,5 +1,4 @@
 import {
-	Box,
 	Button,
 	Center,
 	Divider,
@@ -330,174 +329,177 @@ export default function Community(): JSX.Element {
 							</Container>
 						</VStack>
 
-						<HStack spacing={6}>
-							<Box>
-								<Icon
-									as={RiSpotifyLine}
-									boxSize="100%"
-									color="white"
-									onClick={onOpenSpotify}
-									_hover={{
-										cursor: "pointer",
-									}}
-								/>
+						<HStack
+								flexWrap="wrap"
+							// flexDir={{ sm: "column", md: "row" }}
+							justify="center"
+							align="center"
+						>
+							<Icon
+								as={RiSpotifyLine}
+								boxSize={100}
+								color="white"
+								onClick={onOpenSpotify}
+								_hover={{
+									cursor: "pointer",
+								}}
+							/>
 
-								<Modal
-									isOpen={isOpenSpotify}
-									onClose={onCloseSpotify}
-								>
-									<ModalOverlay />
-									<ModalContent bg="brand.purple.dark">
-										<ModalHeader>Spotify</ModalHeader>
-										<ModalCloseButton />
-										<ModalBody>
-											<Button variant="outline">
-												<Link
-													href="https://open.spotify.com/user/5lkgh8ryszqens1ywo58m5lv8?si=e3b58782d2e94498"
-													isExternal
-												>
-													<Heading size="md">
-														Our Spotify
-													</Heading>
-												</Link>
-											</Button>
+							<Modal
+								isOpen={isOpenSpotify}
+								onClose={onCloseSpotify}
+							>
+								<ModalOverlay />
+								<ModalContent bg="brand.purple.dark">
+									<ModalHeader>Spotify</ModalHeader>
+									<ModalCloseButton />
+									<ModalBody>
+										<Button variant="outline">
+											<Link
+												href="https://open.spotify.com/user/5lkgh8ryszqens1ywo58m5lv8?si=e3b58782d2e94498"
+												isExternal
+											>
+												<Heading size="md">
+													Our Spotify
+												</Heading>
+											</Link>
+										</Button>
 
-											<Text>
-												{" "}
-												Here are links to all of our
-												playlists:{" "}
-											</Text>
+										<Text>
+											{" "}
+											Here are links to all of our
+											playlists:{" "}
+										</Text>
 
-											<Center>
-												<Divider
-													borderColor="white"
-													width="250px"
-													mb="15px"
-												/>
-											</Center>
-
-											<Heading size="md">
-												Lofi Playlists:
-											</Heading>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/5xy112KNO4WBzaxR1tioT9?si=cbf67fcfe567406b&nd=1"
-													isExternal
-												>
-													Study Lofi,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/2qfpV3Cv3LGASgLk5DDIwA?si=df83f8b734784065"
-													isExternal
-												>
-													Jazz Lofi,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/42VuHYE30tU6olqgYCUGj9?si=4cdc91ce894b4d8c"
-													isExternal
-												>
-													Rainy Day Lofi,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/6WGCRBoHJ5NZRg6D3VM7DK?si=4b658781fd54463b"
-													isExternal
-												>
-													Anime Lofi,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/3DGBkdcT236zwEQqsaeiHc?si=c56fda3f5c994ddd"
-													isExternal
-												>
-													Sleeping Lofi,
-												</Link>
-											</Text>
-
+										<Center>
 											<Divider
 												borderColor="white"
-												mt="10px"
-												mb="10px"
+												width="250px"
+												mb="15px"
 											/>
+										</Center>
 
-											<Heading size="md">
-												Pop Playlists:
-											</Heading>
+										<Heading size="md">
+											Lofi Playlists:
+										</Heading>
 
-											<Text>
-												<Link href="https://open.spotify.com/playlist/3KUCDUAke9JNCi3EC3DR4A?si=b84da9bd407d43f2">
-													90s Pop,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/2l050Xz8rnNfYqkyx47WTu?si=040ec727a26844bd"
-													isExternal
-												>
-													2000-2009 Pop,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/1lhX7W0NEvzMSsFCkQfxk4?si=5c16816fc6974f87"
-													isExternal
-												>
-													2010-2015 Pop,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/02oYO9n5qfmULA2deeQ4pR?si=1be766a9b13e4ccf"
-													isExternal
-												>
-													2016-2017 Pop,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/3zXZy9Xh93IY13jpqYf6AU?si=f58dac68060a46a6"
-													isExternal
-												>
-													2018-2019 Pop,
-												</Link>
-											</Text>
-
-											<Text>
-												<Link
-													href="https://open.spotify.com/playlist/1qyZMhC4rC1tc04FdfQr00?si=221b54418a0f4077"
-													isExternal
-												>
-													2020-2021 Pop,
-												</Link>
-											</Text>
-										</ModalBody>
-										<ModalFooter>
-											<Button
-												colorScheme="purple"
-												mr={3}
-												onClick={onCloseSpotify}
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/5xy112KNO4WBzaxR1tioT9?si=cbf67fcfe567406b&nd=1"
+												isExternal
 											>
-												Close
-											</Button>
-										</ModalFooter>
-									</ModalContent>
-								</Modal>
-							</Box>
+												Study Lofi,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/2qfpV3Cv3LGASgLk5DDIwA?si=df83f8b734784065"
+												isExternal
+											>
+												Jazz Lofi,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/42VuHYE30tU6olqgYCUGj9?si=4cdc91ce894b4d8c"
+												isExternal
+											>
+												Rainy Day Lofi,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/6WGCRBoHJ5NZRg6D3VM7DK?si=4b658781fd54463b"
+												isExternal
+											>
+												Anime Lofi,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/3DGBkdcT236zwEQqsaeiHc?si=c56fda3f5c994ddd"
+												isExternal
+											>
+												Sleeping Lofi,
+											</Link>
+										</Text>
+
+										<Divider
+											borderColor="white"
+											mt="10px"
+											mb="10px"
+										/>
+
+										<Heading size="md">
+											Pop Playlists:
+										</Heading>
+
+										<Text>
+											<Link href="https://open.spotify.com/playlist/3KUCDUAke9JNCi3EC3DR4A?si=b84da9bd407d43f2">
+												90s Pop,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/2l050Xz8rnNfYqkyx47WTu?si=040ec727a26844bd"
+												isExternal
+											>
+												2000-2009 Pop,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/1lhX7W0NEvzMSsFCkQfxk4?si=5c16816fc6974f87"
+												isExternal
+											>
+												2010-2015 Pop,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/02oYO9n5qfmULA2deeQ4pR?si=1be766a9b13e4ccf"
+												isExternal
+											>
+												2016-2017 Pop,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/3zXZy9Xh93IY13jpqYf6AU?si=f58dac68060a46a6"
+												isExternal
+											>
+												2018-2019 Pop,
+											</Link>
+										</Text>
+
+										<Text>
+											<Link
+												href="https://open.spotify.com/playlist/1qyZMhC4rC1tc04FdfQr00?si=221b54418a0f4077"
+												isExternal
+											>
+												2020-2021 Pop,
+											</Link>
+										</Text>
+									</ModalBody>
+									<ModalFooter>
+										<Button
+											colorScheme="purple"
+											mr={3}
+											onClick={onCloseSpotify}
+										>
+											Close
+										</Button>
+									</ModalFooter>
+								</ModalContent>
+							</Modal>
 
 							<Link
 								href="https://www.instagram.com/school.simplified/"
@@ -505,7 +507,7 @@ export default function Community(): JSX.Element {
 							>
 								<Icon
 									as={RiInstagramLine}
-									boxSize="100%"
+									boxSize={100}
 									color="white"
 								/>
 							</Link>
@@ -516,7 +518,7 @@ export default function Community(): JSX.Element {
 							>
 								<Icon
 									as={RiFacebookBoxLine}
-									boxSize="100%"
+									boxSize={100}
 									color="white"
 								/>
 							</Link>
@@ -527,7 +529,7 @@ export default function Community(): JSX.Element {
 							>
 								<Icon
 									as={RiDiscordLine}
-									boxSize="100%"
+									boxSize={100}
 									color="white"
 								/>
 							</Link>
@@ -538,7 +540,7 @@ export default function Community(): JSX.Element {
 							>
 								<Icon
 									as={SiTiktok}
-									boxSize="100%"
+									boxSize={90}
 									color="white"
 								/>
 							</Link>
@@ -549,7 +551,7 @@ export default function Community(): JSX.Element {
 							>
 								<Icon
 									as={RiTwitterLine}
-									boxSize="100%"
+									boxSize={100}
 									color="white"
 								/>
 							</Link>
@@ -560,7 +562,7 @@ export default function Community(): JSX.Element {
 							>
 								<Icon
 									as={RiLinkedinBoxLine}
-									boxSize="100%"
+									boxSize={100}
 									color="white"
 								/>
 							</Link>

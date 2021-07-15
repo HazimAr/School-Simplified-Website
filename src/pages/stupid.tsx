037837b868dbe42716b94cfbe4d5b309779607f3
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-14 20:29:10 -0700

    Update stupid.tsx

--- src/pages/stupid.tsx
+++ src/pages/stupid.tsx
@@ -4,7 +4,7 @@ export default function Four(): JSX.Element {
 	return (
 		<Center h="90vh">
 			<Box >
-				<video width="1000" height="1000" autoPlay loop>
+				<video width="1250" height="1250" autoPlay loop>
 					<source src="/rick.mp4" type="video/mp4" />
 				</video>
 				<Heading>

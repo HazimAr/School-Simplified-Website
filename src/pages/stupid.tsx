08d7d0168b0458fd78c5103de07da125222b9210
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-14 20:33:30 -0700

    Update stupid.tsx

--- src/pages/stupid.tsx
+++ src/pages/stupid.tsx
@@ -1,11 +1,14 @@
-import { Center, Heading, Box } from "@chakra-ui/react";
+import { Box, Center, Heading } from "@chakra-ui/react";
 
-export default function Four(): JSX.Element {
+export default function Rick(): JSX.Element {
 	return (
 		<Center h="90vh">
-			<Box >
+			<Box>
 				<video width="1250" height="1250" autoPlay loop>
-					<source src="/rick.mp4" type="video/mp4" />
+					<source
+						src="http://school-simplified.vercel.app/rick.mp4"
+						type="video/mp4"
+					/>
 				</video>
 				<Heading>
 					There are no controls on this video only rick. Sorry

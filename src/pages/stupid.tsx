d5ef3a57cf1709c847eaa10b51bf2d8008f4cf4f
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-14 20:28:45 -0700

    Update stupid.tsx

--- src/pages/stupid.tsx
+++ src/pages/stupid.tsx
@@ -1,12 +1,16 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import { Center, Heading, Box } from "@chakra-ui/react";
 
 export default function Four(): JSX.Element {
 	return (
-		<VStack>
-			<video width="1000" height="1000" autoPlay loop>
-				<source src="/rick.mp4" type="video/mp4" />
-			</video>
-			<Heading>There are no controls on this video only rick. Sorry</Heading>
-		</VStack>
+		<Center h="90vh">
+			<Box >
+				<video width="1000" height="1000" autoPlay loop>
+					<source src="/rick.mp4" type="video/mp4" />
+				</video>
+				<Heading>
+					There are no controls on this video only rick. Sorry
+				</Heading>
+			</Box>
+		</Center>
 	);
 }

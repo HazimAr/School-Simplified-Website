b106d0448f72012739a0bc733600990105701bc5
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-14 21:32:38 -0700

    Update stupid.tsx

--- src/pages/stupid.tsx
+++ src/pages/stupid.tsx
@@ -2,7 +2,14 @@ import { Center } from "@chakra-ui/react";
 
 export default function Four(): JSX.Element {
 	return (
-		<Center position="fixed" h="100vh" w="100vw" mt="-50px" zIndex="99999">
+		<Center
+			position="fixed"
+			h="100vh"
+			w="100vw"
+			mt="-50px"
+			zIndex="99999"
+			style={{ pointerEvents: "none" }}
+		>
 			<iframe
 				width="100%"
 				height="100%"
@@ -11,9 +18,9 @@ export default function Four(): JSX.Element {
 				frameBorder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
 				allowFullScreen
-			></iframe>
+			/>
 		</Center>
 	);
 }
 
-// .ytplayer {pointer-events: none;}
+// .ytplayer

86997022e3d3addb5d14679ec65a55ff8ae4d42b
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-14 20:57:16 -0700

    Update stupid.tsx

--- src/pages/stupid.tsx
+++ src/pages/stupid.tsx
@@ -3,12 +3,12 @@ import ReactPlayer from "react-player";
 
 export default function Four(): JSX.Element {
 	return (
-		<Center h="90vh">
-			<Box>
+		<Center h="70vh" mt="120px">
+			<Box w="80%" >
 				<ReactPlayer
 					url="/rick.mp4"
-					width="848px"
-					height="480px"
+					width="100%"
+					height="100%"
 					// autoPlay
 					loop
 					playing

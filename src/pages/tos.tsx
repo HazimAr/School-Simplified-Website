6b7b1a2a94bcaf03eb58d659ea30a4bb8535e784
IsamarZhu <51845693+IsamarZhu@users.noreply.github.com>
2021-07-16 13:45:57 -0700

    Created ToS and Privacy Pages

---
+++ src/pages/tos.tsx
@@ -0,0 +1,12 @@
+import Container from "@components/container";
+import ContainerInside from "@components/containerInside";
+
+export default function TOS(): JSX.Element {
+	return (
+		<>
+			<Container>
+				<ContainerInside></ContainerInside>
+			</Container>
+		</>
+	);
+}

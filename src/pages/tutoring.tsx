6ea28afd1f844215b1b19e4905f821e33d8c06a2
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-15 21:46:34 -0700

    Update tutoring.tsx

--- src/pages/tutoring.tsx
+++ src/pages/tutoring.tsx
@@ -1,44 +1,24 @@
-import { Box, Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 
 export default function Tutoring(): JSX.Element {
 	return (
 		<Container>
-			<ContainerInside py={5}>
-				<Flex
-					flexDir={{ base: "column", lg: "row" }}
-					alignItems="center"
-					justifyContent="space-between"
-				>
-					<Box>
-						<Heading
-							size="2xl"
-							textAlign={{ base: "center", lg: "left" }}
-						>
-							Uh Oh!
-						</Heading>
-						<Text
-							fontSize="xl"
-							my={3}
-							textAlign={{ base: "center", lg: "left" }}
-						>
-							Looks like you found a page that doesn't exist!
-						</Text>
-						<Text textAlign={{ base: "center", lg: "left" }}>
-							If a page is supposed to be here, please{" "}
-							<Link href="/contact" color="brand.purple.dark">
-								let us know
-							</Link>
-							.
-						</Text>
-					</Box>
-					<Image
-						src="/undraw/404.svg"
-						w={{ base: 200, sm: 300, md: 525 }}
-						mt={{ base: 5, lg: 0 }}
-					/>
-				</Flex>
+			<ContainerInside py={10}>
+				<Center>
+					<iframe
+						title=""
+						src="https://docs.google.com/forms/d/e/1FAIpQLSfT9urTBij4P7Pb3MA0yYw-VfdSJOxLJeMkw9KEn8pAGzbtTQ/viewform?embedded=true"
+						height="750"
+						width="500"
+						frameBorder={0}
+						marginHeight={0}
+						marginWidth={0}
+					>
+						Loading…
+					</iframe>
+				</Center>
 			</ContainerInside>
 		</Container>
 	);

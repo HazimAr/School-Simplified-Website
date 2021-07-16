c64f406b051d0085aacdb3c7e05912a402d97785
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-16 00:23:06 -0700

    Create academics.tsx

---
+++ src/pages/academics.tsx
@@ -0,0 +1,30 @@
+import { getFaqInfo } from "@api/notion";
+import { Flex, Heading } from "@chakra-ui/react";
+import FaqList from "@components/contact/faq-list";
+import Container from "@components/container";
+import ContainerInside from "@components/containerInside";
+import { QAPair } from "types";
+
+export default function Contact({
+	qaPairs,
+}: {
+	qaPairs: QAPair[];
+}): JSX.Element {
+	return (
+		<Container>
+			<ContainerInside>
+				<Flex flexDirection="column" align="stretch">
+					<Heading as="h1" size="xl" mb={3}>
+						FAQ
+					</Heading>
+					<FaqList list={qaPairs} />
+				</Flex>
+			</ContainerInside>
+		</Container>
+	);
+}
+
+export async function getServerSideProps() {
+	const qaPairs = await getFaqInfo();
+	return { props: { qaPairs } };
+}

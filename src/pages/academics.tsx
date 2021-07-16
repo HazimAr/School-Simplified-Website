6644b2a34404ff9f141b916647798415df6da14d
IsamarZhu <51845693+IsamarZhu@users.noreply.github.com>
2021-07-16 01:49:06 -0700

    Update academics.tsx

--- src/pages/academics.tsx
+++ src/pages/academics.tsx
@@ -1,30 +1,48 @@
-import { getFaqInfo } from "@api/notion";
-import { Flex, Heading } from "@chakra-ui/react";
-import FaqList from "@components/contact/faq-list";
+import { Box, Heading, Link, Text, Flex } from "@chakra-ui/react";
+import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
-import { QAPair } from "types";
+import {rounded} from "@styles/theme";
 
-export default function Contact({
-	qaPairs,
-}: {
-	qaPairs: QAPair[];
-}): JSX.Element {
+const sections = [
+	{
+		title: "Chat-Helping & Essay Help",
+		description:
+			"Essay Help: Revising and reviewing essays, providing writing tips to students. Chat-Helping: Monitoring tickets and designated subject channels, answering text questions",
+		link: "https://forms.gle/7Qceft4HgrtLPMmP6",
+	},
+	{
+		title: "Notes & Prep",
+		description:
+			"Notes: Creating notes, finding notes, scanning notes, etc. for school subjects. Prep: Creating resources and holding events regarding high school and college preparation",
+		link: "https://forms.gle/HuwTM42uKQsVAvPTA",
+	},
+	{
+		title: "Tutoring",
+		description:
+			"Provide high-quality, free educational help to high school students across the world",
+		link: "https://forms.gle/AfxmXmGf6fYsW4fKA",
+	},
+];
+
+export default function Academics(): JSX.Element {
 	return (
 		<Container>
 			<ContainerInside>
-				<Flex flexDirection="column" align="stretch">
-					<Heading as="h1" size="xl" mb={3}>
-						FAQ
-					</Heading>
-					<FaqList list={qaPairs} />
+				<Flex justify="center" flexDir="column" align="center">
+					{sections.map((section) => {
+						return (
+							<Box key={section.title} my="30px" maxW="55ch" bg="brand.transparent" rounded={rounded} p="20px">
+								<Heading>{section.title}</Heading>
+								<Text my="10px">{section.description}</Text>
+								<Link href={section.link} isExternal>
+									<Button>Join Now!</Button>
+								</Link>
+							</Box>
+						);
+					})}
 				</Flex>
 			</ContainerInside>
 		</Container>
 	);
-}
-
-export async function getServerSideProps() {
-	const qaPairs = await getFaqInfo();
-	return { props: { qaPairs } };
 }

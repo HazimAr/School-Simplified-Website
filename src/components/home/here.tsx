996e50eac1c010205cb6409bf55c1582e13d6b55
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-15 21:24:57 -0700

    Update here.tsx

--- src/components/home/here.tsx
+++ src/components/home/here.tsx
@@ -1,4 +1,4 @@
-import { Heading, Link, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { Heading, HStack, Link, Text, VStack } from "@chakra-ui/react";
 import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
@@ -9,32 +9,35 @@ export default function Here() {
 	return (
 		<Container bg="brand.transparent" py="50px" mb="25px">
 			<ContainerInside>
-				<SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+				<VStack spacing={10}>
 					<Card
 						title="Free Tutoring"
 						description="Lorem ipsum dolor sit amet consectetur elit. Assumenda minima deleniti Lorem ipsum"
 						button="Get Free Help"
 						href="/tutoring"
 					/>
-					<Card
-						title="Essay Revisions"
-						description="Our team consists of exceptional writers, all that go through a rigorous process to establish themselves as verified proofreaders. Simply fill out the form with basic details involving your essay and a proofreader will make the necessary edits"
-						button="Get Free Help"
-						href="/essay"
-					/>
-					{/* <Card
+
+					<HStack justify="center" spacing={10}>
+						<Card
+							title="Essay Revisions"
+							description="Our team consists of exceptional writers, all that go through a rigorous process to establish themselves as verified proofreaders. Simply fill out the form with basic details involving your essay and a proofreader will make the necessary edits"
+							button="Get Free Help"
+							href="/essay"
+						/>
+						<Card
+							title="Resources"
+							description="School Simplified has hundreds of high-quality notes ranging across many different school subjects. Our library of notes will consistently be updating to cover as many subjects as possible"
+							button="Get Free Help"
+							href="/resources"
+						/>
+						{/* <Card
 						title="HS/College Prep"
 						description="Lorem ipsum dolor sit amet consectetur elit. Assumenda minima deleniti Lorem ipsum"
 						button="Get Free Help"
 						href="/prep"
 					/> */}
-					<Card
-						title="Resources"
-						description="School Simplified has hundreds of high-quality notes ranging across many different school subjects. Our library of notes will consistently be updating to cover as many subjects as possible"
-						button="Get Free Help"
-						href="/resources"
-					/>
-				</SimpleGrid>
+					</HStack>
+				</VStack>
 			</ContainerInside>
 		</Container>
 	);
@@ -55,8 +58,8 @@ function Card({
 		<VStack
 			rounded={rounded}
 			bg="brand.transparent2"
-			minH={250}
-			h="fit-content"
+			minH={300}
+			w="100%"
 			justify="center"
 			boxShadow="lg"
 			p={10}

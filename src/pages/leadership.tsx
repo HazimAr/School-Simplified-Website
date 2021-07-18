b142066fddfaeff360d2b4426dfa86dd5cfc475a
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-17 18:19:23 -0700

    Update leadership.tsx

--- src/pages/leadership.tsx
+++ src/pages/leadership.tsx
@@ -2,10 +2,12 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 import {
 	Box,
+	Center,
 	Divider,
 	Flex,
 	Heading,
 	HStack,
+	Image,
 	Stack,
 	Text,
 	VStack,
@@ -128,6 +130,39 @@ export default function About(): JSX.Element {
 						</Box>
 						<Flex justifyContent="center" flexWrap="wrap">
 							{leadership.map((staff, i: number) => {
+								if (i == 4) {
+									return (
+										<Flex
+											flexDir="column"
+											p="15px"
+											m="5px"
+											align="center"
+										>
+											<Center
+												w="500px"
+												h="500px"
+												backgroundColor="brand.transparent"
+												rounded="50px"
+											>
+												<Image
+													rounded="30px"
+													display="block"
+													boxSize="450px"
+													objectFit="cover"
+													src={staff.img}
+												/>
+											</Center>
+											<Box maxW="200px" py="5px">
+												<Heading size="md">
+													{staff.name}
+												</Heading>
+												<Text size="sm" flexWrap="wrap">
+													{staff.title}
+												</Text>
+											</Box>
+										</Flex>
+									);
+								}
 								return (
 									<StaffCard
 										title={staff.title}

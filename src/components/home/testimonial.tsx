97853c97c66ca722021a4a94e239a05819efb8cd
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-17 20:07:59 -0700

    Update testimonial.tsx

--- src/components/home/testimonial.tsx
+++ src/components/home/testimonial.tsx
@@ -14,7 +14,7 @@ const Reviews = [
 		title: "Sophomore",
 	},
 	{
-		review: "I've had multiple tutoring sessions with him because he is an amazing tutor, and he even helped me revise an essay that got me into a program at Stanford, as well as get a 100% on my final. He even made some practice questions just for me and was super patient and talked me through every question I had. Overall, I've noticed a significant improvement in my quiz scores and understanding of the material.",
+		review: "I've had multiple tutoring sessions with him and he's helped me revise an essay that got me into a program at Stanford, as well as get a 100% on my final with the help of some practice questions he made for me. Overall, he's an amazing tutor and I've noticed a significant improvement in my scores and understanding of the material.",
 		name: "Adam",
 		title: "Freshman",
 	},

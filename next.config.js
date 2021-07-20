56ab621fe81a62b4ebf9588370955786f155868f
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-20 12:43:58 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
-module.exports = {};
+module.exports = {
+	eslint: {
+		// Warning: Dangerously allow production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+};

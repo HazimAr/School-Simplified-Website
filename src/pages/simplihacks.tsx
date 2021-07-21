6a8817464993434a6a508568a71cea544fe6a0df
whyroland <42717361+whyroland@users.noreply.github.com>
2021-07-21 09:36:47 -0400

    Created Simplihacks Page

---
+++ src/pages/simplihacks.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/layout";
+import Container from "@components/container";
+import ContainerInside from "@components/containerInside"; 
+import React from "react";
+
+export default function Simplihacks(): JSX.Element {
+    return (
+        <>
+            <Container>
+                <ContainerInside>
+                    
+                </ContainerInside>
+            </Container>
+        </>
+    );
+}

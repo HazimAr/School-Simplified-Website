932e8f0faf7734e722616638487ac2bf663d1b06
whyroland <42717361+whyroland@users.noreply.github.com>
2021-07-21 12:08:36 -0400

    Simplihacks Staff Array

--- src/components/simplihacks/simplihackscard.tsx
+++ src/components/simplihacks/simplihackscard.tsx
@@ -1,9 +1,41 @@
+/* eslint-disable import/no-default-export */
+import { Box, Center, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 
-export default function SimplihacksCard(): JSX.Element {
+type SimplihacksCard = {
+	name: string;
+	title: string;
+	img: string;
+};
+
+export default function SimplihacksCard({
+	title,
+	name,
+	img,
+}: SimplihacksCard): JSX.Element {
 	return (
-		<>
-			
-		</>
+		<Flex flexDir="column" p="15px" m="5px" align="center">
+			<Center
+				w="200px"
+				h="200px"
+				backgroundColor="brand.transparent"
+				rounded="50px"
+			>
+				<Image
+					rounded="30px"
+					display="block"
+					boxSize="165px"
+					objectFit="cover"
+					alt="staff member"
+					src={img}
+				/>
+			</Center>
+			<Box maxW="200px" py="5px">
+				<Heading size="md">{name}</Heading>
+				<Text size="sm" flexWrap="wrap">
+					{title}
+				</Text>
+			</Box>
+		</Flex>
 	);
 }
--- src/components/simplihacks/staff.tsx
+++ src/components/simplihacks/staff.tsx
@@ -1,5 +1,113 @@
+import {
+	Box,
+	Divider,
+	Flex,
+	Heading,
+	HStack,
+	Stack,
+	Text,
+	VStack,
+} from "@chakra-ui/react";
+import Container from "@components/container";
+import ContainerInside from "@components/containerInside";
 import React from "react";
+import { Person } from "types";
+
+const staff: Person[] = [
+	{
+		name: "Roland Yang",
+		title: "Lead Organizer",
+		img: "/simplihacks/staff/roland.png",
+	},
+	{
+		name: "Ahaan Limaye",
+		title: "Lead Organizer",
+		img: "/simplihacks/staff/ahaan.png",
+	},
+	{
+		name: "Aryan Pal",
+		title: "Lead Organizer",
+		img: "/simplihacks/staff/aryan.png",
+	},
+	{
+		name: "Eric Lin",
+		title: "Organizer",
+		img: "/simplihacks/staff/eric.jpg",
+	},
+	{
+		name: "Makayla Ma",
+		title: "Design",
+		img: "/simplihacks/staff/kayla.jpg",
+	},
+	{
+		name: "Amanda Zhong",
+		title: "Design",
+		img: "/simplihacks/staff/amanda.jpg",
+	},
+	{
+		name: "Giselle Galvan",
+		title: "Design",
+		img: "/simplihacks/staff/giselle.jpg",
+	},
+	{
+		name: "Riddhi Shedge",
+		title: "Outreach",
+		img: "/simplihacks/staff/riddhi.jpg",
+	},
+	{
+		name: "Ryan Badal",
+		title: "Outreach",
+		img: "/simplihacks/staff/ryan.png",
+	},
+	{
+		name: "Sanjit Ravichandran",
+		title: "Outreach",
+		img: "/simplihacks/staff/default.png",
+	},
+	{
+		name: "Trey Brower",
+		title: "Events Coordinator",
+		img: "/simplihacks/staff/trey.jpg",
+	},
+	{
+		name: "Nicole Li",
+		title: "Events Coordinator",
+		img: "/simplihacks/staff/nicole.png",
+	},
+	{
+		name: "Shivam Bhatia",
+		title: "Judge",
+		img: "/simplihacks/staff/shivam.jpg",
+	},
+	{
+		name: "David Medvedik",
+		title: "Judge",
+		img: "/simplihacks/staff/david.jpg",
+	},
+	{
+		name: "Anshuman Girdhar",
+		title: "Judge",
+		img: "/simplihacks/staff/ansh.jpg",
+	},
+	{
+		name: "Nathanael Ma",
+		title: "Judge",
+		img: "/simplihacks/staff/default.png",
+	},
+];
 
 export default function Staff(): JSX.Element {
-	return <></>;
+	return (
+        <>
+            <Container py={19}>
+                <ContainerInside>
+                    <Box>
+                        <Heading as="h1" m={5}>
+                            Staff
+                        </Heading>
+                    </Box>
+                </ContainerInside>
+            </Container>
+        </>
+    );
 }

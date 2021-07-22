8b3d3a6684862da3eeb0e1d7e4d6667dd7151691
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-21 19:24:55 -0700

    Partners page

---
+++ src/pages/partners.tsx
@@ -0,0 +1,40 @@
+import { Box, Heading, HStack, Text } from "@chakra-ui/react";
+import Container from "@components/container";
+import ContainerInside from "@components/containerInside";
+import Image from "next/image";
+import { ReactElement } from "react";
+
+export default function partners(): ReactElement {
+	return (
+		<Container>
+			<ContainerInside p={10}>
+				<Box>
+					<Heading>Our Partners</Heading>
+					<Text>
+						School Simplified is proudly partnered by the companies
+						below. Each logo is clickable and links through to the
+						sponsor's own website - please support these companies
+						in any way you can, as a thank you for their incredible
+						support of School Simplified.
+					</Text>
+				</Box>
+				<HStack
+					justify="center"
+					flexDir={{ base: "column", md: "row" }}
+                    mt={5}
+				>
+					<Image
+						width="300px"
+						height="125px"
+						src="/partners/slingshot.png"
+					/>
+					<Image
+						width="400px"
+						height="100px"
+						src="/partners/versatile.png"
+					/>
+				</HStack>
+			</ContainerInside>
+		</Container>
+	);
+}

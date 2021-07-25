58dcbd6a70a8fb59b044c5be17af068b1ddf4882
whyroland <42717361+whyroland@users.noreply.github.com>
2021-07-25 00:46:42 -0400

    Moved Art Component Into Separate File

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -1,6 +1,5 @@
 import { getArtInfo } from "@api/notion";
 import {
-	AspectRatio,
 	Divider,
 	Heading,
 	HStack,
@@ -9,15 +8,7 @@ import {
 	Stack,
 	Text,
 	UnorderedList,
-	useDisclosure,
 	VStack,
-	Modal,
-	ModalOverlay,
-	ModalContent,
-	ModalHeader,
-	ModalBody,
-	ModalCloseButton,
-	ModalFooter
 } from "@chakra-ui/react";
 import Button from "@components/button";
 import Container from "@components/container";
@@ -26,6 +17,7 @@ import NextLink from "@components/nextChakra";
 import { rounded } from "@styles/theme";
 import React from "react";
 import SocialMedias from "@components/community/socialmedias"
+import Art from "@components/community/art"
 import { ArtData, SpotifyLink } from "types";
 
 const mainPlaylists: SpotifyLink[] = [
@@ -52,7 +44,6 @@ export default function Community({
 }: {
 	artInfo: ArtData;
 }): JSX.Element {
-	const { isOpen, onOpen, onClose } = useDisclosure();
 	return (
 		<>
 			<Container py={10}>
@@ -63,122 +54,7 @@ export default function Community({
 							flexDir={{ base: "column", md: "row" }}
 							spacing={10}
 						>
-							<Stack boxSize="80%" mb={25}>
-								<Stack
-									backgroundColor="brand.transparent2"
-									rounded={rounded}
-									boxShadow="lg"
-								>
-									<AspectRatio ratio={1}>
-										<Image
-											src={artInfo.image}
-											alt="art winner"
-											boxSize="100%"
-											rounded={rounded}
-											objectFit="cover"
-											boxShadow="lg"
-											transition="all 0.2s ease"
-											_hover={{
-												cursor: "pointer",
-												transform: "scale(0.95)",
-											}}
-											onClick={onOpen}
-										/>
-									</AspectRatio>
-
-									<Modal
-										isOpen={isOpen}
-										onClose={onClose}
-										isCentered
-										motionPreset="slideInRight"
-									>
-										<ModalOverlay />
-										<ModalContent
-											bg="brand.purple.dark"
-											textAlign="left"
-										>
-											<ModalHeader>
-												Monthly Art Contest Winner
-											</ModalHeader>
-											<ModalCloseButton />
-											<ModalBody>
-												This month's art contest prompt
-												was:{" "}
-												<Text>
-													{artInfo.monthlyPrompt}
-												</Text>
-												<Text mt={5}>
-													The winner is:
-													<Heading size="sm">
-														{artInfo.name}
-													</Heading>
-												</Text>
-												<Heading size="sm" mt={5}>
-													Artist Description:
-												</Heading>
-												<Text>
-													{artInfo.description}
-												</Text>
-												<Heading size="sm" mt={5}>
-													{artInfo.socialMedia.map(
-														(
-															social,
-															key: number
-														) => {
-															return (
-																<Text
-																	mt={5}
-																	key={key}
-																>
-																	Follow{" "}
-																	{
-																		artInfo.name
-																	}{" "}
-																	on{" "}
-																	{
-																		social.media
-																	}
-																	:{" "}
-																	{social.link ? (
-																		<NextLink
-																			href={
-																				social.link
-																			}
-																		>
-																			{
-																				social.tag
-																			}
-																		</NextLink>
-																	) : (
-																		<Text>
-																			{
-																				social.tag
-																			}
-																		</Text>
-																	)}
-																</Text>
-															);
-														}
-													)}
-												</Heading>
-											</ModalBody>
-
-											<ModalFooter>
-												<Button
-													mr={3}
-													onClick={onClose}
-												>
-													Close
-												</Button>
-											</ModalFooter>
-										</ModalContent>
-									</Modal>
-								</Stack>
-
-								<Text textAlign="center">
-									Winning artwork from the latest event!
-								</Text>
-							</Stack>
+							<Art artInfo={artInfo}/>
 
 							<Stack w="100%">
 								<Heading size="lg">Our Community</Heading>

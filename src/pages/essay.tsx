366c51d8532ee9b5cb3c05138cd3da333776a4f9
IsamarZhu <51845693+IsamarZhu@users.noreply.github.com>
2021-08-10 14:04:51 -0700

    Revert "Made form smaller on mobile"
    This reverts commit 711cdf239dde223acc640246c74065817dae3b6b.

--- src/pages/essay.tsx
+++ src/pages/essay.tsx
@@ -1,4 +1,4 @@
-import { Center, Box } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 import Head from "next/head";
@@ -10,21 +10,19 @@ export default function Essay(): JSX.Element {
 				<title>School Simplified | Essay</title>
 			</Head>
 			<Container>
-				<ContainerInside py={10} justify="center">
+				<ContainerInside py={10}>
 					<Center>
-						<Box w={{ base: "230px", sm: "400px", lg: "600px" }}>
-							<iframe
-								title=""
-								src="https://docs.google.com/forms/d/e/1FAIpQLSe_8Dt4HL-UpI-JeTzTS-9K8bZE5WzdsQB38nZ7L915unEGCg/viewform?embedded=true"
-								height="750"
-								width="100%"
-								frameBorder={0}
-								marginHeight={0}
-								marginWidth={0}
-							>
-								Loading…
-							</iframe>
-						</Box>
+						<iframe
+							title=""
+							src="https://docs.google.com/forms/d/e/1FAIpQLSe_8Dt4HL-UpI-JeTzTS-9K8bZE5WzdsQB38nZ7L915unEGCg/viewform?embedded=true"
+							height="750"
+							width="600"
+							frameBorder={0}
+							marginHeight={0}
+							marginWidth={0}
+						>
+							Loading…
+						</iframe>
 					</Center>
 				</ContainerInside>
 			</Container>

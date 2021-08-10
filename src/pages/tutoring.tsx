49cb4b597a01dd0ad3cf63b2fb191a73400727c0
IsamarZhu <51845693+IsamarZhu@users.noreply.github.com>
2021-08-10 14:04:43 -0700

    Revert "Made tutor form smaller on mobile"
    This reverts commit fc63909dd7a714d4948ddd675e88ee85b7eaf532.

--- src/pages/tutoring.tsx
+++ src/pages/tutoring.tsx
@@ -1,4 +1,4 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 import Head from "next/head";
@@ -12,19 +12,17 @@ export default function Tutoring(): JSX.Element {
 			<Container>
 				<ContainerInside py={10}>
 					<Center>
-						<Box w={{ base: "230px", sm: "400px", lg: "600px" }}>
-							<iframe
-								title=""
-								src="https://docs.google.com/forms/d/e/1FAIpQLSfT9urTBij4P7Pb3MA0yYw-VfdSJOxLJeMkw9KEn8pAGzbtTQ/viewform?embedded=true"
-								height="750"
-								width="100%"
-								frameBorder={0}
-								marginHeight={0}
-								marginWidth={0}
-							>
-								Loading…
-							</iframe>
-						</Box>
+						<iframe
+							title=""
+							src="https://docs.google.com/forms/d/e/1FAIpQLSfT9urTBij4P7Pb3MA0yYw-VfdSJOxLJeMkw9KEn8pAGzbtTQ/viewform?embedded=true"
+							height="750"
+							width="600"
+							frameBorder={0}
+							marginHeight={0}
+							marginWidth={0}
+						>
+							Loading…
+						</iframe>
 					</Center>
 				</ContainerInside>
 			</Container>

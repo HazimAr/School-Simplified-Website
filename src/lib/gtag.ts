e55f9908724e2a900321933226b889870f69c892
Hazim Arafa <hazimarafa80@gmail.com>
2021-08-25 18:47:42 -0700

    no more strict typing

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,7 +1,8 @@
-import { GA_TRACKING_ID } from "@src/config.ts";
+import { GA_TRACKING_ID } from "config";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
+	//@ts-ignore
 	window.gtag("config", GA_TRACKING_ID, {
 		page_location: url,
 	});
@@ -9,6 +10,7 @@ export const pageview = (url) => {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
+	//@ts-ignore
 	window.gtag("event", action, {
 		event_category: category,
 		event_label: label,

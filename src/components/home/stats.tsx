ddadd8a6acdc9960a2b26ae79b2aa6e91281ced3
Hazim Arafa <hazimarafa80@gmail.com>
2021-09-01 16:32:49 -0700

    Update stats.tsx

--- src/components/home/stats.tsx
+++ src/components/home/stats.tsx
@@ -1,46 +1,61 @@
-import { HStack } from "@chakra-ui/layout";
+import { Box, Flex, Text, VStack } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 import CountUp from "react-countup";
+import { BsPeopleFill } from "react-icons/bs";
+import { FaFlag, FaGlobe } from "react-icons/fa";
 
 export default function Stats() {
 	return (
-		<Container py={5}>
+		<Container py={20}>
 			<ContainerInside>
-				<HStack
+				<Flex
 					spacing={{ base: 0, md: 5 }}
-					justify={{ base: "flex-start", md: "center" }}
-					flexDir={{ base: "column", md: "row" }}
+					justify={{ base: "center", md: "space-around" }}
+					flexWrap="wrap"
+					flexDir={{ base: "column", sm: "row" }}
 					textAlign="center"
 					fontSize={{ base: "lg", md: "xl" }}
 				>
-					<CountUp
-						end={500}
-						suffix="M+ People Reached"
-						duration={3}
-						useEasing
-					/>
-					<CountUp
-						end={60}
-						suffix="K+ Community Members"
-						duration={3}
-						useEasing
-					/>
-					<CountUp
-						end={400}
-						suffix="+ Staff Members"
-						duration={3}
-						useEasing
-					/>
-					<CountUp
-						end={147}
-						suffix=" Countries"
-						duration={3}
-						useEasing
-					/>
-					<CountUp end={1} suffix=" Mission" duration={3} useEasing />
-				</HStack>
+					<StatsIcon Icon={BsPeopleFill}>
+						<CountUp end={500} suffix="M+" duration={3} useEasing />
+						<Text>People Reached</Text>
+					</StatsIcon>
+					<StatsIcon Icon={BsPeopleFill}>
+						<CountUp end={60} suffix="K+" duration={3} useEasing />
+						<Text>Community Members</Text>
+					</StatsIcon>
+					<StatsIcon Icon={FaFlag}>
+						<CountUp end={1} duration={3} useEasing />
+						<Text>Mission</Text>
+					</StatsIcon>
+					<StatsIcon Icon={BsPeopleFill}>
+						<CountUp end={400} suffix="+" duration={3} useEasing />
+						<Text>Staff Members</Text>
+					</StatsIcon>
+					<StatsIcon Icon={FaGlobe}>
+						<CountUp end={147} suffix="" duration={3} useEasing />
+						<Text>Countries</Text>
+					</StatsIcon>
+
+					{/* <Stack>
+						<Button>Volunteer</Button>
+						<Button>Partner</Button>
+						<Button></Button>
+					</Stack> */}
+				</Flex>
 			</ContainerInside>
 		</Container>
+	);
+}
+
+function StatsIcon({ children, Icon, ...props }) {
+	return (
+		<VStack m="5px" {...props}>
+			<Box display={{ base: "none", md: "block" }}>
+				<Icon size="4rem" color="#fff" />
+			</Box>
+			{children}
+		</VStack>
 	);
 }

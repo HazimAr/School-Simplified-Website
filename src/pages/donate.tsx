76a2c456d4101d6806125bc13e54e7e2fb8701a4
Hazim <hazimarafa80@gmail.com>
2021-10-10 12:37:21 -0700

    Create donate.tsx

---
+++ src/pages/donate.tsx
@@ -0,0 +1,17 @@
+import { Center } from "@chakra-ui/react";
+
+export default function Donate() {
+	return (
+		<Center>
+			<iframe
+				src="https://master.d3o53wn77q6k5v.amplifyapp.com/"
+				// height="650"
+				// width="500"
+				frameBorder="0"
+				allowTransparency
+			>
+				{" "}
+			</iframe>
+		</Center>
+	);
+}

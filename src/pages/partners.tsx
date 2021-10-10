d66e2ca3988596619aa2eb82a0018d92f511a989
Hazim <hazimarafa80@gmail.com>
2021-10-10 15:28:41 -0700

    partners

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -9,12 +9,7 @@ import {
 	PopoverContent,
 	PopoverHeader,
 	PopoverTrigger,
-	SimpleGrid,
-	Tab,
-	TabList,
-	TabPanel,
-	TabPanels,
-	Tabs,
+	HStack,
 	Text,
 	useDisclosure,
 	Wrap,
@@ -76,125 +71,7 @@ export default function partners(): ReactElement {
 			<Container>
 				<ContainerInside>
 					<Heading mb={10}>Our Partners</Heading>
-					<Tabs variant="line" colorScheme="purple" align="center">
-						<Box backgroundColor="#D8D6EC">
-							<TabList
-								justify="center"
-								align="center"
-								color="#8287BE"
-							>
-								<Tab
-									_selected={{
-										borderBottom: "5px solid #8287BE",
-									}}
-									pt={3}
-								>
-									<Heading
-										fontSize={{
-											base: 13,
-											md: 20,
-											lg: 30,
-										}}
-										color="#8287BE"
-									>
-										Academic Partners
-									</Heading>
-								</Tab>
-								<Tab
-									_selected={{
-										borderBottom: "5px solid #8287BE",
-									}}
-									pt={3}
-								>
-									<Heading
-										fontSize={{
-											base: 13,
-											md: 20,
-											lg: 30,
-										}}
-										color="#8287BE"
-									>
-										Community Partners
-									</Heading>
-								</Tab>
-								<Tab
-									_selected={{
-										borderBottom: "5px solid #8287BE",
-									}}
-									pt={3}
-								>
-									<Heading
-										fontSize={{
-											base: 13,
-											md: 20,
-											lg: 30,
-										}}
-										color="#8287BE"
-									>
-										Donors &amp; Sponsors
-									</Heading>
-								</Tab>
-							</TabList>
-						</Box>
-
-						<TabPanels mt={10}>
-							<TabPanel>
-								<SimpleGrid
-									columns={{ sm: 2, md: 3 }}
-									spacing={6}
-									minChildWidth={{
-										base: 170,
-										sm: 140,
-										md: 250,
-										lg: 200,
-									}}
-									spacingY={0}
-								>
-									<Cell
-										alt="Slingshot"
-										src="/partners/slingshot.png"
-										desc="Slingshot is a organization that places teenage prodigies at startups out of Techstars, YC, Carnegie Mellon, Stanford, MIT, and more, offering students access to the real world of technology."
-									/>
-									<Cell
-										alt="Versatile"
-										src="/partners/versatile.png"
-										desc="Versatile Node is an organization geared towards providing cheap, fast and reliable hosting for all your needs! Versatile offers resources range from minecraft hosting, VPS hosting, to web hosting."
-									/>
-									<Cell
-										alt="Deloitte"
-										src="/partners/deloitte.png"
-										desc="Deloitte US is the largest professional services organization in the United States. With more than 100,000 professionals, Deloitte provides audit and assurance, tax, consulting, and risk and financial advisory services to a broad cross-section of the largest corporations and governmental agencies."
-									/>
-								</SimpleGrid>
-							</TabPanel>
-							<TabPanel>
-								<SimpleGrid
-									columns={{ sm: 2, md: 3 }}
-									spacing={6}
-									minChildWidth={{
-										base: 170,
-										sm: 170,
-										md: 250,
-										lg: 300,
-									}}
-									spacingY={0}
-								></SimpleGrid>
-							</TabPanel>
-							<TabPanel>
-								<SimpleGrid
-									columns={{ sm: 2, md: 3 }}
-									spacing={6}
-									minChildWidth={{
-										base: 170,
-										sm: 170,
-										md: 250,
-										lg: 300,
-									}}
-									spacingY={0}
-								></SimpleGrid>
-							</TabPanel>
-						</TabPanels>
-					</Tabs>
+					<HStack></HStack>
 				</ContainerInside>
 			</Container>
 		</>

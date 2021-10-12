dfb382e26abfb07cd7fb12c60cb52c4f5b8db1e9
Hazim Arafa <hazimarafa80@gmail.com>
2021-10-12 11:35:20 -0700

    rough draft

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -14,14 +14,17 @@ import {
 	useDisclosure,
 	Wrap,
 	WrapItem,
+	SimpleGrid,
 } from "@chakra-ui/react";
 import StyledButton from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 import NextLink from "@components/nextChakra";
-import { ReactElement } from "react";
+import { useState } from "react";
 
-export default function partners(): ReactElement {
+export default function PartnersPage() {
+	const [partner, setPartner] = useState(0);
+
 	return (
 		<>
 			<Container>
@@ -71,20 +74,107 @@ export default function partners(): ReactElement {
 			<Container>
 				<ContainerInside>
 					<Heading mb={10}>Our Partners</Heading>
-					<HStack></HStack>
+					<HStack
+						bg="brand.transparent"
+						borderRadius="25px"
+						spacing={0}
+					>
+						{partners.map((partnerMap, index) => {
+							return (
+								<PartnerButton
+									onClick={() => {
+										setPartner(index);
+									}}
+									bg={
+										partner == index && "brand.transparent2"
+									}
+									key={partnerMap.name}
+								>
+									{partnerMap.name}
+								</PartnerButton>
+							);
+						})}
+					</HStack>
+					<SimpleGrid
+						columns={{ sm: 2, md: 3 }}
+						spacing={6}
+						minChildWidth={{
+							base: 170,
+							sm: 140,
+							md: 250,
+							lg: 200,
+						}}
+						spacingY={0}
+					>
+						{partners[partner].data.map((partnerData) => {
+							return (
+								<Cell
+									key={partnerData.name}
+									alt={partnerData.name}
+									src={partnerData.src}
+									desc={partnerData.description}
+								/>
+							);
+						})}
+					</SimpleGrid>
 				</ContainerInside>
 			</Container>
 		</>
 	);
 }
 
-type ImageCell = {
-	src: string;
-	alt: string;
-	desc: string;
-};
+const partners = [
+	{
+		name: "Academic Partners",
+		data: [
+			{
+				name: "Slingshot",
+				description:
+					"Slingshot is a non-profit organization that provides technical assistance to students in the field of computer science.",
+				src: "/partners/slingshot.png",
+			},
+			{
+				name: "Versatile Node",
+				description:
+					"Versatile Node is an organization geared towards providing cheap, fast and reliable hosting for all your needs! Versatile offers resources range from minecraft hosting, VPS hosting, to web hosting.",
+				src: "/partners/versatile.png",
+			},
+			{
+				name: "Deloitte",
+				description:
+					"Deloitte US is the largest professional services organization in the United States. With more than 100,000 professionals, Deloitte provides audit and assurance, tax, consulting, and risk and financial advisory services to a broad cross-section of the largest corporations and governmental agencies.",
+				src: "/partners/deloitte.png",
+			},
+		],
+	},
+	{
+		name: "Community Partners",
+		data: [],
+	},
+	{
+		name: "Donors & Sponsors",
+		data: [],
+	},
+];
 
-function Cell({ src, alt, desc }: ImageCell): JSX.Element {
+function PartnerButton({ onClick, children, bg }) {
+	return (
+		<Box
+			as="button"
+			onClick={onClick}
+			w="100%"
+			py={3}
+			_hover={{ background: "brand.transparent2" }}
+			transition="background 0.2s ease-in"
+			bg={bg}
+			borderRadius="25px"
+		>
+			{children}
+		</Box>
+	);
+}
+
+function Cell({ src, alt, desc }): JSX.Element {
 	const graceTime: number = 250;
 
 	const { isOpen, onOpen, onClose } = useDisclosure();

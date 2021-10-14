f3ec68b1276338d4d23ef297443c269ed0fa47ab
Hazim Arafa <hazimarafa80@gmail.com>
2021-10-14 08:35:34 -0700

    Update activities.tsx

--- src/pages/activities.tsx
+++ src/pages/activities.tsx
@@ -1,5 +1,17 @@
 import ContainerBackground from "@components/containerBackground";
-import { VStack, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import {
+	VStack,
+	Heading,
+	HStack,
+	Image,
+	Text,
+	Accordion,
+	AccordionItem,
+	AccordionButton,
+	AccordionIcon,
+	AccordionPanel,
+	Box,
+} from "@chakra-ui/react";
 import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
@@ -27,8 +39,52 @@ export default function Activities() {
 							<Button>Club President</Button>
 						</VStack>
 						<Image src="/timmy/16.png" w="600px" />
-						
 					</HStack>
+				</ContainerInside>
+			</Container>
+			<Container>
+				<ContainerInside>
+					<Heading>FAQ</Heading>
+
+					<Accordion>
+						<AccordionItem>
+							<h2>
+								<AccordionButton>
+									<Box flex="1" textAlign="left">
+										Section 1 title
+									</Box>
+									<AccordionIcon />
+								</AccordionButton>
+							</h2>
+							<AccordionPanel pb={4}>
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation
+								ullamco laboris nisi ut aliquip ex ea commodo
+								consequat.
+							</AccordionPanel>
+						</AccordionItem>
+
+						<AccordionItem>
+							<h2>
+								<AccordionButton>
+									<Box flex="1" textAlign="left">
+										Section 2 title
+									</Box>
+									<AccordionIcon />
+								</AccordionButton>
+							</h2>
+							<AccordionPanel pb={4}>
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation
+								ullamco laboris nisi ut aliquip ex ea commodo
+								consequat.
+							</AccordionPanel>
+						</AccordionItem>
+					</Accordion>
 				</ContainerInside>
 			</Container>
 		</>

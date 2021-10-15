2c39ac1df60a6320feb4128fc12b993cf9378720
Hazim <hazimarafa80@gmail.com>
2021-10-15 15:30:31 -0700

    Update intro.tsx

--- src/components/home/intro.tsx
+++ src/components/home/intro.tsx
@@ -1,35 +1,26 @@
-import { Box, Flex, Heading, Text, VStack } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, VStack, Center } from "@chakra-ui/react";
 import ContainerBackground from "@components/containerBackground";
 import ContainerInside from "@components/containerInside";
 import CountUp from "react-countup";
-import { BsFillPersonFill, BsPeopleFill } from "react-icons/bs";
-import { FaFlag, FaGlobe, FaHandHoldingHeart } from "react-icons/fa";
-
-const spacing = 20;
 
 export default function Intro() {
 	return (
-		<ContainerBackground src="/timmy/lofi_timmy.png">
+		<ContainerBackground src="/timmy/lofi_timmy.png" py={20}>
 			<ContainerInside>
-				<Flex justify="center" align="center" mt={50}>
-					<Box maxW="60ch" my={`${spacing}px`}>
-						<Heading fontWeight="extrabold">
-							School Simplified
+				<Center>
+					<VStack spacing={10} maxW="60ch">
+						<Heading fontWeight="extrabold" size="3xl">
+							School Simplified:
 							<br /> We Inspire Learning
 						</Heading>
 						<Text my="10px">
 							Through learning, we empower the next generation to
 							revolutionize the future. School Simplified is a
 							nonprofit organization dedicated to learning done
-							equally, fairly and with meaning. And we don’t just
-							mean education, but also business, trades, passions,
-							leadership, and life.
+							equally, fairly and with meaning.
 						</Text>
-						{/* <NextLink href="/notes">
-							<Button>Get Free Help</Button>
-						</NextLink> */}
-					</Box>
-				</Flex>
+					</VStack>
+				</Center>
 				<Flex
 					spacing={{ base: 0, md: 5 }}
 					justify={{ base: "center", md: "space-around" }}
@@ -37,40 +28,60 @@ export default function Intro() {
 					flexDir={{ base: "column", sm: "row" }}
 					textAlign="center"
 					fontSize={{ base: "lg", md: "xl" }}
+					mt="50px"
 				>
-					<StatsIcon Icon={BsPeopleFill}>
-						<CountUp end={500} suffix="M+" duration={3} useEasing />
+					<VStack>
+						<Heading>
+							<CountUp
+								end={500}
+								suffix="M+"
+								duration={3}
+								useEasing
+							/>
+						</Heading>
 						<Text>Impression</Text>
-					</StatsIcon>
-					<StatsIcon Icon={FaHandHoldingHeart}>
-						<CountUp end={60} suffix="K+" duration={3} useEasing />
+					</VStack>
+					<VStack>
+						<Heading>
+							<CountUp
+								end={60}
+								suffix="K+"
+								duration={3}
+								useEasing
+							/>
+						</Heading>
 						<Text>Community Members</Text>
-					</StatsIcon>
-					<StatsIcon Icon={FaFlag}>
-						<CountUp end={1} duration={3} useEasing />
+					</VStack>
+					<VStack>
+						<Heading>
+							<CountUp end={1} duration={3} useEasing />
+						</Heading>
 						<Text>Mission</Text>
-					</StatsIcon>
-					<StatsIcon Icon={BsFillPersonFill}>
-						<CountUp end={400} suffix="+" duration={3} useEasing />
+					</VStack>
+					<VStack>
+						<Heading>
+							<CountUp
+								end={400}
+								suffix="+"
+								duration={3}
+								useEasing
+							/>
+						</Heading>
 						<Text>Staff Members</Text>
-					</StatsIcon>
-					<StatsIcon Icon={FaGlobe}>
-						<CountUp end={147} suffix="" duration={3} useEasing />
+					</VStack>
+					<VStack>
+						<Heading>
+							<CountUp
+								end={147}
+								suffix=""
+								duration={3}
+								useEasing
+							/>
+						</Heading>
 						<Text>Countries</Text>
-					</StatsIcon>
+					</VStack>
 				</Flex>
 			</ContainerInside>
 		</ContainerBackground>
-	);
-}
-
-function StatsIcon({ children, Icon, ...props }) {
-	return (
-		<VStack m="5px" {...props}>
-			<Box display={{ base: "none", md: "block" }}>
-				<Icon size="4rem" color="#fff" />
-			</Box>
-			{children}
-		</VStack>
 	);
 }

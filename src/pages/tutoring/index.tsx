6d52632b66e444ad07091526ce58a4539b126ccc
daniel56483 <56704631+daniel56483@users.noreply.github.com>
2021-10-25 15:14:33 -0700

    Revert "Merge branch 'v2' of https://github.com/HazimAr/School-Simplified into v2"
    This reverts commit 0e263cda604c6c7e21dedccba6cc2aa2669b18e5, reversing
    changes made to 47de82af4091a5cc3f86006aa31bac828b87b6af.

--- src/pages/tutoring/index.tsx
+++ src/pages/tutoring/index.tsx
@@ -1,26 +1,9 @@
-import { Center, } from "@chakra-ui/react";
-import ContainerInside from "@components/containerInside";
-import ContainerBackground from "@components/containerBackground";
+import { Center, Text } from "@chakra-ui/react";
 
-export default function Tutoring(): JSX.Element {
-	return (
-		<ContainerBackground src="/timmy/lofi_timmy.png" py="50px" alt="Create Timmy Background">
-			<ContainerInside py={10}>
-				<Center>
-					<iframe
-						title=""
-						src="https://docs.google.com/forms/d/e/1FAIpQLSfT9urTBij4P7Pb3MA0yYw-VfdSJOxLJeMkw9KEn8pAGzbtTQ/viewform?embedded=true"
-						height="600"
-						width="600"
-						frameBorder={0}
-						marginHeight={0}
-						marginWidth={0}
-					>
-						Loading…
-					</iframe>
-				</Center>
-			</ContainerInside>
-        </ContainerBackground>
-	);
+export default function Tutor(){
+    return(
+        <Center>
+        <Text>Hi There im a piece of text</Text>
+        </Center>
+    )
 }
-

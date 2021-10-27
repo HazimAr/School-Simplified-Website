6a8ce2867ffd709bab8397f25fd4f8e0cc60b1e8
Hazim <hazimarafa80@gmail.com>
2021-10-26 21:44:33 -0700

    Update clubs.tsx

--- src/components/home/clubs.tsx
+++ src/components/home/clubs.tsx
@@ -1,11 +1,6 @@
-import { Flex, Heading, Text, VStack } from "@chakra-ui/react";
-import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 import Clubs from "@components/activities/changing_slide";
-import NextLink from "@components/nextChakra";
-
-import React from "react";
 
 export default function Here() {
 	return (

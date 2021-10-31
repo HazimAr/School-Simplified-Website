d7cc19ba9428e2cfb1529bb7d3c0e552e662b49f
Hazim <hazimarafa80@gmail.com>
2021-10-31 11:52:16 -0700

    Update intro.tsx

--- src/components/home/intro.tsx
+++ src/components/home/intro.tsx
@@ -5,8 +5,8 @@ import CountUp from "react-countup";
 
 export default function Intro() {
 	return (
-		// <ContainerBackground src="/timmy/lofi_timmy.png" py="50px">
-		<ContainerBackground src="/timmy/lofi_timmy.png" h="90vh">
+		<ContainerBackground src="/timmy/lofi_timmy.png" py="50px">
+	
 			<ContainerInside>
 				<Center>
 					<VStack spacing={10} maxW="60ch">

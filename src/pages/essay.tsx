775406a3121363155eb4f5523c067e2f2878dd42
Hazim <hazimarafa80@gmail.com>
2021-10-31 11:23:26 -0700

    essay

---
+++ src/pages/essay.tsx
@@ -0,0 +1,25 @@
+import { Center, } from "@chakra-ui/react";
+import ContainerInside from "@components/containerInside";
+import ContainerBackground from "@components/containerBackground";
+
+export default function Essay(): JSX.Element {
+	return (
+		<ContainerBackground src="/timmy/lofi_timmy.png" py="50px" alt="Create">
+			<ContainerInside py={10}>
+				<Center>
+					<iframe
+						title=""
+						src="https://docs.google.com/forms/d/e/1FAIpQLSe_8Dt4HL-UpI-JeTzTS-9K8bZE5WzdsQB38nZ7L915unEGCg/viewform?embedded=true"
+						height="600"
+						width="600"
+						frameBorder={0}
+						marginHeight={0}
+						marginWidth={0}
+					>
+						Loading…
+					</iframe>
+				</Center>
+			</ContainerInside>
+		</ContainerBackground>
+	);
+}

7ee81e2bc039d3b68cdf79ea90a9e7488fee235b
Farhan Uddin <farhanuddin2006@gmail.com>
2021-11-01 11:19:41 -0400

    Finished the Marketing Applications Page
    This page includes buttons which lead to google forms, where people can apply for different marketing positions at School Simplified.

--- src/pages/volunteer/marketing.tsx
+++ src/pages/volunteer/marketing.tsx
@@ -1,18 +1,214 @@
 import Hero from "@components/volunteering/hero";
 
+import { HStack, Center, VStack, Image, Text } from "@chakra-ui/react";
+
+import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
-export default function Marketing() {
+import NextLink from "@components/nextChakra";
+import {
+	BsPalette,
+	BsTools,
+	BsMegaphoneFill,
+	BsInstagram,
+} from "react-icons/bs";
+import { GiPencil } from "react-icons/gi";
+import { FaLightbulb, FaTiktok } from "react-icons/fa";
+
+export default function Projects() {
 	return (
 		<>
-			<Container>
+			<Container h="300px">
 				<ContainerInside>
 					<Hero heading="Marketing Applications" />
 				</ContainerInside>
 			</Container>
 
-			<Container>
-				<ContainerInside></ContainerInside>
+			<Container bg="#8F9CFE" h="400px">
+				<ContainerInside>
+					<Center>
+						<HStack>
+							<Image
+								src="/timmy/timmywithbackground.png"
+								alt="timmy with pink background"
+								w="350px"
+								mr="70px"
+							/>
+							<HStack spacing="20px">
+								<VStack spacing="20px">
+									<NextLink
+										isExternal
+										href="https://forms.gle/QnwAAWpPs5XcWfzr8"
+										_hover={{ TextDecoration: "none" }}
+									>
+										<Button
+											bg="#7283FE"
+											md="100px"
+											w="350px"
+											h="50px"
+											textAlign="left"
+											borderRadius="35px"
+										>
+											<HStack>
+												<BsPalette />
+												<Text>Designer</Text>
+											</HStack>
+										</Button>
+									</NextLink>
+
+									<NextLink
+										isExternal
+										href="https://forms.gle/TgNLPu9bYARxt8Ks5"
+										_hover={{ TextDecoration: "none" }}
+									>
+										<Button
+											bg="#7283FE"
+											md="100px"
+											w="350px"
+											h="50px"
+											textAlign="left"
+											borderRadius="35px"
+										>
+											<HStack>
+												<BsTools />
+												<Text>Discord Editor</Text>
+											</HStack>
+										</Button>
+									</NextLink>
+									<NextLink
+										isExternal
+										href="https://forms.gle/e8EArXqZYXZj6vxr6"
+										_hover={{ TextDecoration: "none" }}
+									>
+										<Button
+											bg="#7283FE"
+											md="100px"
+											w="350px"
+											h="50px"
+											textAlign="left"
+											borderRadius="35px"
+										>
+											<HStack>
+												<BsMegaphoneFill />
+												<Text>
+													Public Relations Specialist
+												</Text>
+											</HStack>
+										</Button>
+									</NextLink>
+									<NextLink
+										isExternal
+										href="https://forms.gle/grxWXCRPjPAV9izLA"
+										_hover={{ TextDecoration: "none" }}
+									>
+										<Button
+											bg="#7283FE"
+											md="100px"
+											w="350px"
+											h="50px"
+											textAlign="left"
+											borderRadius="35px"
+										>
+											<HStack>
+												<FaLightbulb />
+												<Text>
+													Vice President of Global
+													Marketing
+												</Text>
+											</HStack>
+										</Button>
+									</NextLink>
+								</VStack>
+								<VStack spacing="20px">
+									<NextLink
+										isExternal
+										href="https://forms.gle/qe4LaxJsnZNigP3c7"
+										_hover={{ TextDecoration: "none" }}
+									>
+										<Button
+											bg="#7283FE"
+											md="100px"
+											w="350px"
+											h="50px"
+											textAlign="left"
+											borderRadius="35px"
+										>
+											<HStack>
+												<BsInstagram />
+												<Text>
+													Instagram Content Creator
+												</Text>
+											</HStack>
+										</Button>
+									</NextLink>
+
+									<NextLink
+										isExternal
+										href="https://forms.gle/qe4LaxJsnZNigP3c7"
+										_hover={{ TextDecoration: "none" }}
+									>
+										<Button
+											bg="#7283FE"
+											md="100px"
+											w="350px"
+											h="50px"
+											textAlign="left"
+											borderRadius="35px"
+										>
+											<HStack>
+												<FaTiktok />
+												<Text>
+													TikTok Content Creator
+												</Text>
+											</HStack>
+										</Button>
+									</NextLink>
+									<NextLink
+										isExternal
+										href="https://forms.gle/qe4LaxJsnZNigP3c7"
+										_hover={{ TextDecoration: "none" }}
+									>
+										<Button
+											bg="#7283FE"
+											md="100px"
+											w="350px"
+											h="50px"
+											textAlign="left"
+											borderRadius="35px"
+										>
+											<HStack>
+												<GiPencil />
+												<Text>Blog Writer</Text>
+											</HStack>
+										</Button>
+									</NextLink>
+									<NextLink
+										isExternal
+										href="https://forms.gle/fWDh8MMN2tsKo6gy7"
+										_hover={{ TextDecoration: "none" }}
+									>
+										<Button
+											bg="#7283FE"
+											md="100px"
+											w="350px"
+											h="50px"
+											textAlign="left"
+											borderRadius="35px"
+										>
+											<HStack>
+												<FaLightbulb />
+												<Text>
+													President of Marketing,
+													Editorial
+												</Text>
+											</HStack>
+										</Button>
+									</NextLink>
+								</VStack>
+							</HStack>
+						</HStack>
+					</Center>
+				</ContainerInside>
 			</Container>
 		</>
 	);

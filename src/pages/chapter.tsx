432d493c503673d3055c564546379506d78f2e97
Hazim Arafa <hazimarafa80@gmail.com>
2021-11-01 13:00:03 -0700

    Update chapter.tsx

--- src/pages/chapter.tsx
+++ src/pages/chapter.tsx
@@ -1,5 +1,12 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 export default function Chapter() {
-	return <Box></Box>;
+	return (
+		<Box>
+			<Heading>
+				We are currently not taking applications right now. Please check
+				back with us later
+			</Heading>
+		</Box>
+	);
 }

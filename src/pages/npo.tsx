0ebee6e52e2e68b28d8fd665047572715b11be12
Hazim <hazimarafa80@gmail.com>
2021-11-01 15:27:37 -0700

    Update npo.tsx

--- src/pages/npo.tsx
+++ src/pages/npo.tsx
@@ -140,3 +140,21 @@
 // 	);
 // }
 
+import { Heading, Center } from "@chakra-ui/react";
+import Container from "@components/container";
+import ContainerInside from "@components/containerInside";
+
+export default function Chapter() {
+	return (
+		<Container>
+			<ContainerInside>
+				<Center h="100vh">
+					<Heading>
+						We are currently not taking applications right now.
+						Please check back with us later
+					</Heading>
+				</Center>
+			</ContainerInside>
+		</Container>
+	);
+}

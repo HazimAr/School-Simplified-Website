d0e52152c1afad53f4ad47be6caf2dd3a049aa04
Hazim <hazimarafa80@gmail.com>
2021-11-01 18:33:37 -0700

    ye

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home({ listing }: { listing: BlogListing[] }) {
 	return (
 		<>
 			<Intro />
-			<Here />
+			{/* <Here /> */}
 			<Testimonial />
 			{/* <Blog listing={listing} /> */}
 			{/* <Code /> */}

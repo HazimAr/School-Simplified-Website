43f72d7d9939e4d4f5e564a43b1509cd377ca1c2
Yelloo5191 <76452706+Yelloo5191@users.noreply.github.com>
2021-11-02 23:05:51 -0700

    completed chapter page

--- src/pages/chapter.tsx
+++ src/pages/chapter.tsx
@@ -1,19 +1,49 @@
-import { Heading, Center } from "@chakra-ui/react";
+import { 
+	Heading, 
+	HStack, 
+	Text, 
+	VStack, 
+	Image,
+} from "@chakra-ui/react";
 import Container from "@components/container";
+import Button from "@components/button";
 import ContainerInside from "@components/containerInside";
+import NextLink from "@components/nextChakra";
 
 export default function Chapter() {
 	return (
-		<Container>
-			<ContainerInside>
-				<Center h="100vh">
-					<Heading>
-						We are not currently taking applications.
-						Please check back with us later,
-						we look forward to working with you!
+		<>
+			<Container>
+				<ContainerInside>
+					<Heading mt="10vh">
+						Start Your Own Chapter
 					</Heading>
-				</Center>
-			</ContainerInside>
-		</Container>
+					<NextLink
+						isExternal
+						href="https://docs.google.com/forms/d/e/1FAIpQLSfs8_MYtRqA2vq5X5hm9hu-QjLb7PczSJ6wCBTz6vuXOkVKjg/viewform?usp=sf_link"
+						_hover={{ textDecoration: "none" }}
+						p={15}
+					>
+						<Button mt="2vh">Applications</Button>
+					</NextLink>
+				</ContainerInside>
+			</Container>
+			<Container boxShadow=" 0 0 100px 100px rgba(108, 125, 254, 0.71) " py={50} my={50} bg="linear-gradient(180deg, rgba(90, 96, 173, 0.71) 0%, rgba(108, 125, 254, 0.71) 100%);">
+				<ContainerInside>
+					<HStack>
+						<VStack alignItems="start">
+							<Heading my={5}>Taking Chapters to the Next Level.</Heading>
+							<Text fontSize="18px" textAlign="left">
+								School Simplified chapters are the perfect blend of flexibility and rigor. 
+								With the chapters program, motivated students from around the world are able to gain leadership experience, 
+								conduct service projects, and help others progress in their academics. Chapters can offer service, tutoring, 
+								essay revision, and college resources. Each chapter is free to focus on one or more areas of interest.
+							</Text>
+						</VStack>
+						<Image src="/timmy/timmy_chapter.png"  display={{ base: "none", md: "block" }} alt="Timmy with globe" />
+					</HStack>
+				</ContainerInside>
+			</Container>
+		</>
 	);
 }

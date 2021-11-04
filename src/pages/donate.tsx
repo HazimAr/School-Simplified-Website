55393a69a53728ba5c79c18757d6e2e725612b4f
Yelloo5191 <76452706+Yelloo5191@users.noreply.github.com>
2021-11-04 11:53:53 -0700

    Fix donate page?

--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -1,28 +1,30 @@
-import { Container, Heading, Center } from "@chakra-ui/react";
+import { Container, Heading, VStack } from "@chakra-ui/react";
 
 export default function Donate() {
 	return (
 			<Container py={19}>
-				<Center my="20px">
-					<Heading as="h1" size="4xl" mb={8}>
-						Donate
-					</Heading>
-					<Heading as="h2" size="md" color="white">
-						School simplified doesn't charge anything for our
-						services, so we rely on your donations! Consider
-						donating to us.
-					</Heading>
+				<ContainerInside>
+					<VStack my="20px">
+						<Heading as="h1" size="4xl" mb={8}>
+							Donate
+						</Heading>
+						<Heading as="h2" size="md" color="white">
+							School simplified doesn't charge anything for our
+							services, so we rely on your donations! Consider
+							donating to us.
+						</Heading>
 
-					<iframe
-						src="https://master.d3o53wn77q6k5v.amplifyapp.com/"
-						height="700"
-						width="600"
-						frameBorder="0"
-						allowTransparency
-					>
-						{" "}
-					</iframe>
-				</Center>
+						<iframe
+							src="https://master.d3o53wn77q6k5v.amplifyapp.com/"
+							height="700"
+							width="600"
+							frameBorder="0"
+							allowTransparency
+						>
+							{" "}
+						</iframe>
+					</VStack>
+				</ContainerInside>
 			</Container>
 	);
 }

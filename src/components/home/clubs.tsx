e9067bfde2c8837144cd0fde76d85862f8e1934b
Gui Guy <jedandeganwang1999@gmail.com>
2021-12-04 00:04:23 -0600

    Fixed up clubs to be more like the Figma

--- src/components/home/clubs.tsx
+++ src/components/home/clubs.tsx
@@ -1,12 +1,19 @@
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 import Clubs from "@components/activities/changing_slide";
+import { Flex, Heading } from "@chakra-ui/layout";
 
 export default function Here() {
 	return (
-		<Container pt="100px">
-			<ContainerInside>
-				<Clubs />
+		<Container bg="linear-gradient(180deg, rgba(123, 139, 255, 0.6) 0%, rgba(197, 203, 255, 0.6) 100%)">
+			<ContainerInside textAlign="left">
+				<Flex flexDir="column" py={12}>
+					<Heading size="lg">Join Our Clubs</Heading>
+					<Heading size="md" fontWeight="normal" mb={7}>
+						Student Activites Program
+					</Heading>
+					<Clubs />
+				</Flex>
 			</ContainerInside>
 		</Container>
 	);

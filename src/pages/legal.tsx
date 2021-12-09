f3d5d8938ac6f9f1be0deefdd1e354f0f451a652
Hazim <hazimarafa80@gmail.com>
2021-12-09 12:26:43 -0800

    Update legal.tsx

--- src/pages/legal.tsx
+++ src/pages/legal.tsx
@@ -11,7 +11,7 @@ export default function Marketing() {
 		<>
 			<Container h="300px">
 				<ContainerInside>
-					<Hero heading="Marketing Applications" />
+					<Hero heading="School Simplified Legal" />
 				</ContainerInside>
 			</Container>
 
@@ -35,11 +35,13 @@ export default function Marketing() {
 										_hover={{ TextDecoration: "none" }}
 									>
 										<Button
-											bg="#7283FE"
+											bg="#585EAB"
 											md="100px"
 											w="300px"
+											rounded={5}
 											minH="60px"
 											textAlign="left"
+											shadow="lg"
 										>
 											<HStack>
 												<Text>{legal.name}</Text>
@@ -61,9 +63,18 @@ const legals = [
 		name: "Bylaws",
 		link: "https://drive.google.com/file/d/1Xf8RyJoHZDWMuiWgnjbxEIkyoEbIpPiZ/view?usp=sharing",
 	},
+
 	{
 		name: "Articles of Incorporation",
 		link: "https://drive.google.com/file/d/1Xf8RyJoHZDWMuiWgnjbxEIkyoEbIpPiZ/view?usp=sharing",
+	},
+	{
+		name: "501(c)3 Determination Letter",
+		link: "https://drive.google.com/file/d/1v4IUPJLlCIIUQhQuaOjcBBWJgJbYBUb0/view?usp=sharing",
+	},
+	{
+		name: "Employee Identification Number",
+		link: "https://drive.google.com/file/d/19qZLprsRDo25LjzT4Xq4H2Ows4M_MsI6/view?usp=sharing",
 	},
 	{
 		name: "Nomination Committee",
@@ -84,13 +95,5 @@ const legals = [
 	{
 		name: "Anti-Discrimination and Anti-Harassment Policy",
 		link: "https://drive.google.com/file/d/1Hio0Tst49m55VcT_8OPdXwSQyP7ANI2W/view?usp=sharing",
-	},
-	{
-		name: "Employee Identification Number",
-		link: "https://drive.google.com/file/d/19qZLprsRDo25LjzT4Xq4H2Ows4M_MsI6/view?usp=sharing",
-	},
-	{
-		name: "501(c)3 Determination Letter",
-		link: "https://drive.google.com/file/d/1v4IUPJLlCIIUQhQuaOjcBBWJgJbYBUb0/view?usp=sharing",
 	},
 ];

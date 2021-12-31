dc483a186aae73ee347693a7417676c792a8e10b
Hazim Arafa <hazimarafa80@gmail.com>
2021-12-30 16:02:48 -0800

    Revert "map"
    This reverts commit 310606383cf2255c911be455263e03a98388410f.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,126 +1,200 @@
 import {
 	Box,
 	Heading,
+	HStack,
 	Image,
 	Stack,
 	Text,
 	VStack,
-	HStack,
-	Link,
-	Center,
-	Spacer,
-	Flex,
 } from "@chakra-ui/react";
 import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
-import NextLink from "@components/nextChakra";
-import { motion } from "framer-motion";
-import { slideInDown, slideInUp } from "@styles/animations";
+import NextChakraLink from "@components/nextChakra";
 
-export default function Projects(): JSX.Element {
+export default function Four(): JSX.Element {
 	return (
-		<Container
-			py={16}
-			bg="linear-gradient(180deg, rgba(99, 106, 204, 0.9) 0%, rgba(126, 134, 230, 0.747) 100%, rgba(122, 131, 247, 0.9) 100%)"
+		<>
+			<Container py="50px">
+				<ContainerInside>
+					<HStack>
+						<Image
+							display={{ base: "none", md: "block" }}
+							src="timmy/timmy_flying.png"
+							w="230px"
+							alt="Timmy with beanie"
+						/>
+						<VStack
+							textAlign="left"
+							align="start"
+							mb="10px"
+							justify="space-between"
+						>
+							<Heading fontSize="35px">
+								Start Your Project Today
+							</Heading>
+							<Text textAlign="left" fontSize="22px">
+								Take the chance to showcase your capability,
+								creativity, and ideas while receiving community
+								service hours and possibly becoming a project
+								manager!
+							</Text>
+							<Box pt="15px">
+								<Button timmysrc="timmy/timmy_paper_icon.svg">
+									Propose Project
+								</Button>
+							</Box>
+						</VStack>
+					</HStack>
+				</ContainerInside>
+			</Container>
+			<Container
+				py="50px"
+				bg="linear-gradient(180deg, rgba(140, 147, 228, 0.4615) 0%, rgba(167, 178, 255, 0.71) 100%);"
+			>
+				<ContainerInside>
+					<HStack>
+						<VStack spacing={8}>
+							<Questions
+								title="What is a project?"
+								text="A project is a temporary endeavor that creates a unique product or service consistent with our mission of inspiring learning while fostering an environment attuned to adapting and improving through customer input."
+								image="timmy/timmy_ruler.png"
+								w={{ base: "100%", md: "75%" }}
+								rounded={30}
+							/>
+							<Stack
+								spacing={8}
+								direction={{ base: "column", md: "row" }}
+							>
+								<Questions
+									title="What can my project be?"
+									text="Projects can include almost anything from hackathons, advocacy campaigns, competitive startups, and more!"
+									image={null}
+									width="100%"
+								/>
+								<Questions
+									title="Do I lead my own project?"
+									text="That is up to you! Once your proposal is approved, you can choose to be the project manager or recommend a friend to take over while you are in the team helping with the project."
+									image={null}
+									width="100%"
+								/>
+							</Stack>
+						</VStack>
+					</HStack>
+				</ContainerInside>
+			</Container>
+			<Container py="50px">
+				<ContainerInside>
+					<Heading fontSize="35px" textAlign="left">
+						SimpliHacks
+					</Heading>
+					<Heading fontSize="22px" textAlign="left">
+						Previous Projects
+					</Heading>
+					<Text py="15px" fontSize="22px" textAlign="left">
+						An example would be Simplihacks, one of the largest
+						hackathons in the US that earned over $100k in prizes!
+						It is a 2-day virtual hackathon, brought to you by
+						School Simplified, that will re-occur in February of
+						2022 so stay tuned!
+					</Text>
+					<Stack
+						direction={{ base: "column-reverse", lg: "row" }}
+						align="start"
+						spacing={50}
+					>
+						<Winner
+							image="simplihacks/winners/signslate.png"
+							title="Signslate"
+							award="1st Place Winner from June 2021"
+							winners="Ayush Agarwal, Krushay Bhavsar"
+							link="https://devpost.com/software/signslate"
+						/>
+						<VStack align="start" spacing={5}>
+							<Text fontSize="22px" textAlign="left">
+								If this is your first hackathon or you have no
+								experience with coding, do not worry!
+								Participants of all levels to compete are
+								welcomed to join our great workshops that can
+								get you started on your computer science
+								journey.
+								{<br />}
+								{<br />}
+								Want to learn how to make cool websites and
+								apps? Or maybe you just want to learn how to
+								code for fun. We will help you do just that.
+							</Text>
+							<Button timmysrc="timmy/timmy_scroll_icon.png">
+								More Information
+							</Button>
+						</VStack>
+					</Stack>
+				</ContainerInside>
+			</Container>
+		</>
+	);
+}
+
+function Questions({ title, text, image = null, ...props }) {
+	return (
+		<Box
+			overflow="hidden"
+			rounded={20}
+			bg="rgba(90, 96, 173, .8)"
+			boxShadow="0px 5px 5px rgba(0, 0, 0, 0.2)"
+			{...props}
 		>
-			<ContainerInside maxWidth="79%" textAlign="left">
-				<Center>
-					<Flex flexDir="column">
-						<Stack>
-							<motion.div
-								initial="initial"
-								whileInView="onView"
-								variants={slideInDown()}
-							>
-								<Heading size="lg" mb={{ base: 2, md: 0 }}>
-									Start Your{" "}
-									<Link href="/projects">Project</Link> Today
-								</Heading>
+			<HStack align="end">
+				<VStack m="20px" align="start">
+					<Heading textAlign="left" fontSize="22px">
+						{title}
+					</Heading>
+					<Text textAlign="left" fontSize="18px">
+						{text}
+					</Text>
+				</VStack>
+				<Image
+					height="150px"
+					display={{ base: "none", md: "block" }}
+					src={image}
+				/>
+			</HStack>
+		</Box>
+	);
+}
 
-								<Text mt={2} mb={3} fontSize={19}>
-									Take the chance to showcase your capability,
-									creativity, and ideas while receiving
-									community service hours and possibly
-									becoming a project manager!
-								</Text>
-							</motion.div>
-							<motion.div
-								initial="initial"
-								whileInView="onView"
-								variants={slideInUp()}
-							>
-								<Box rounded={15} bg="#5E65B7" px={7} py={5}>
-									<HStack spacing={0}>
-										<VStack align="start" textAlign="left">
-											<Heading fontSize={24}>
-												What is a Project?
-											</Heading>
-											<Text mt={6} textAlign="left">
-												A project is a temporary
-												initiative undertaken to further
-												School Simplified's mission of
-												revolutionizing the future
-												through learning!
-											</Text>
-											<Text mt={6} textAlign="left">
-												Projects can include almost
-												anything from hackathons,
-												advocacy campaings, competitive
-												startups, and more!
-											</Text>
-
-											<Stack
-												direction={{
-													base: "column",
-													sm: "row",
-												}}
-												spacing="5%"
-												p={15}
-											>
-												<NextLink
-													isExternal
-													href="https://forms.gle/fyP8RPFqtWNC4AY68"
-													_hover={{
-														textDecoration: "none",
-													}}
-												>
-													<Button timmysrc="/timmy/10.png">
-														Propose Project
-													</Button>
-												</NextLink>
-
-												<NextLink
-													isExternal
-													href="/projects"
-													_hover={{
-														textDecoration: "none",
-													}}
-												>
-													<Button timmysrc="/timmy/17.png">
-														Learn More
-													</Button>
-												</NextLink>
-											</Stack>
-										</VStack>
-										<Spacer maxW={{ md: 5, lg: 20 }} />
-										<Image
-											src="/timmy/projectstimmy.png"
-											w={{ md: 250, lg: 350 }}
-											display={{
-												base: "none",
-												md: "none",
-												lg: "block",
-											}}
-											alt="Timmy with a computer"
-										/>
-									</HStack>
-								</Box>
-							</motion.div>
-						</Stack>
-					</Flex>
-				</Center>
-			</ContainerInside>
-		</Container>
+function Winner({
+	image,
+	title,
+	link,
+	award,
+	winners,
+}: {
+	image: string;
+	title: string;
+	link: string;
+	award: string;
+	winners: string;
+}) {
+	return (
+		<Box
+			w="100%"
+			minW="350px"
+			borderRadius="5px"
+			bg="rgba(90, 96, 173, .8)"
+		>
+			<Image w="100%" src={image} />
+			<VStack align="start" p="15px">
+				<NextChakraLink target="_blank" href={link}>
+					<Heading fontSize="22px">{title}</Heading>
+				</NextChakraLink>
+				<Text textAlign="left" fontSize="18px">
+					{award}
+				</Text>
+				<Heading textAlign="left" fontSize="18px">
+					{winners}
+				</Heading>
+			</VStack>
+		</Box>
 	);
 }

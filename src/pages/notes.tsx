27922f0d7410c07066f8ea9ffeb688c5cb504127
shionkorsak <90345258+shionkorsak@users.noreply.github.com>
2022-01-02 23:06:01 -0600

    edits of testimonials on home page and new notes page header

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,6 +1,5 @@
 import { getSubjects } from "@api/notion";
-import { Heading, HStack, Image, Stack, Text } from "@chakra-ui/react";
-import ContainerBackground from "@components/containerBackground";
+import { Heading, HStack, Image, Stack, Text, Container, Center, Spacer, Flex } from "@chakra-ui/react";
 import ContainerInside from "@components/containerInside";
 import NotesViewer from "@components/resources/notesViewer";
 // import NotesSection from "@components/resources/notes_section";
@@ -9,32 +8,39 @@ import { AllSubjects } from "types";
 export default function Resources({ subjects }: AllSubjects): JSX.Element {
 	return (
 		<>
-			<ContainerBackground src="/timmy/lofi_timmy.png" mb={8}>
-				<ContainerInside py={16}>
-					<HStack spacing={8}>
+			<Container 
+			maxW="%100"
+			py={10}
+			bg="linear-gradient(180deg, rgba(90, 96, 173, 0.71) 0%, rgba(131, 145, 255, 0.71) 100%)" >
+				<ContainerInside mb={8} maxW="%80">
+					<Center>
+						<HStack>
 						<Image
 							src="/timmy/blogtimmy.png"
 							alt="Timmy holding a book"
-							h="33vw"
+							w={{ sm: 80, md: 120, lg: 180 }}
+							display={{ base: "none", md: "block" }}
 						/>
-						<Stack textAlign="left" flex={1} spacing={4}>
-							<Heading as="h1" mb={3} size="3xl">
-								Notes
-							</Heading>
-							<Text fontSize={24}>
-								All the top-notch notes you'll ever want for
-								your academic needs!
-							</Text>
-							<Text fontSize={24}>
-								To get started, select a subject from the
-								buttons below!
-							</Text>
-						</Stack>
-					</HStack>
+							<Spacer maxW={{ md: 5, lg: 20 }} />
+							<Flex align="stretch">
+							<Stack align="start" textAlign="left">
+								<Heading size="lg">
+									Notes
+								</Heading>
+								<Text mt={6}>
+									All the top-notch notes you'll ever want for 
+									your academic needs!
+								</Text>
+								<Text mt={6}>
+									To get started, select a subject from the buttons below!
+								</Text>
+							</Stack>
+							</Flex>
+						</HStack>
+					</Center>
 				</ContainerInside>
-			</ContainerBackground>
-
-			<NotesViewer subjects={subjects} />
+				<NotesViewer subjects={subjects} />
+			</Container>
 
 			{/* <NotesSection subjects={subjects} /> */}
 		</>

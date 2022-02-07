6ccb2304c4c79fbe09cdf440daec6e50836bf3da
Gui Guy <jedandeganwang1999@gmail.com>
2022-02-07 12:44:00 -0600

    Fix compile issues

--- src/config.tsx
+++ src/config.tsx
@@ -24,6 +24,7 @@ const META: MetaType = {
 	lang: "en-us",
 	// description:
 	// 	"School Simplified is a virtually run nonprofit that aims to spread educational equality by providing free educational services, products, and opportunities for teenagers. We provide free tutoring, free Essay Revision, free notes, free college preparation, and a community where like-minded teenagers can meet and form life-lasting connections.",
+	description: null,
 	url: "https://schoolsimplified.tech",
 	img: "/logo.png",
 };

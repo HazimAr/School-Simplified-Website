94fe92e19331be4ac31339ead83296c3b1ce5f09
Gui Guy <jedandeganwang1999@gmail.com>
2022-02-08 19:25:49 -0600

    Staff card now creates a modal for execs with a bio when clicked

--- src/components/staffcard.tsx
+++ src/components/staffcard.tsx
@@ -3,14 +3,25 @@ import {
 	Box,
 	Center,
 	Heading,
+	HStack,
 	Image,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
 	Stack,
 	StackProps,
 	Text,
+	useDisclosure,
+	VStack,
 } from "@chakra-ui/react";
 import React from "react";
 import { Executive } from "types";
 import { parseText } from "util/parse_notion";
+import Button from "./button";
 // import { RiBoxingLine } from "react-icons/ri";
 
 type StaffCardProps = {
@@ -21,60 +32,111 @@ export default function StaffCard({
 	staff: { name, image, title, tagline, biography },
 	...props
 }: StaffCardProps): JSX.Element {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const taglineElement = tagline?.length ? (
+		<Text
+			fontSize={14}
+			as="q"
+			position="relative"
+			_before={{ position: "absolute", top: 0, left: 0 }}
+			_after={{ position: "absolute", bottom: "-0.8em", right: 0 }}
+		>
+			{tagline.map((s) =>
+				React.cloneElement(parseText(s), {
+					key: s.plain_text + JSON.stringify(s.annotations),
+				})
+			)}
+		</Text>
+	) : null;
+
 	return (
 		<Stack p={4} m={1} spacing={2} maxW={{ base: 200, lg: 300 }} {...props}>
 			<Center p={17} backgroundColor="brand.transparent" rounded={50}>
-				<Box
-					rounded={30}
-					overflow="hidden"
-					position="relative"
-					style={{ aspectRatio: "1" }}
-				>
+				{biography?.length ? (
+					<Box rounded={30} overflow="hidden">
+						<Image
+							alt={"Picture of " + name}
+							objectFit="cover"
+							// overflow="hidden"
+							// position="relative"
+							style={{ aspectRatio: "1" }}
+							// objectPosition="top"
+							src={image?.url ?? "/staff/default.png"}
+							transition="0.25s ease transform"
+							_hover={{
+								transform: "scale(1.1)",
+								cursor: "pointer",
+							}}
+							onClick={onOpen}
+						/>
+					</Box>
+				) : (
 					<Image
 						alt={"Picture of " + name}
 						objectFit="cover"
-						h="100%"
-						w="100%"
+						rounded={30}
+						// overflow="hidden"
+						// position="relative"
+						style={{ aspectRatio: "1" }}
 						// objectPosition="top"
 						src={image?.url ?? "/staff/default.png"}
+						onClick={onOpen}
 					/>
-					{biography?.length ? (
-						<Center
-							position="absolute"
-							h="100%"
-							w="100%"
-							top={0}
-							left={0}
-							opacity={0}
-							background="#0007"
-							transition="0.2s ease all"
-							_hover={{ opacity: 1 }}
-						>
-							<Text fontSize={14}>
-								{biography.map((s) =>
-									React.cloneElement(parseText(s), {
-										key:
-											s.plain_text +
-											JSON.stringify(s.annotations),
-									})
-								)}
-							</Text>
-						</Center>
-					) : null}
-				</Box>
+				)}
 			</Center>
 			<Box>
 				<Heading size="md">{name}</Heading>
 				<Text flexWrap="wrap">{title}</Text>
 			</Box>
-			{tagline?.length ? (
-				<Text fontSize={14}>
-					{tagline.map((s) =>
-						React.cloneElement(parseText(s), {
-							key: s.plain_text + JSON.stringify(s.annotations),
-						})
-					)}
-				</Text>
+
+			{taglineElement}
+			{biography?.length ? (
+				<Modal
+					isOpen={isOpen}
+					onClose={onClose}
+					motionPreset="slideInBottom"
+					size="3xl"
+					isCentered
+				>
+					<ModalOverlay />
+					<ModalContent>
+						<ModalHeader>{name}'s Bio</ModalHeader>
+						<ModalCloseButton />
+
+						<ModalBody>
+							<HStack spacing={5}>
+								<VStack flex={1} align="stretch">
+									<Image
+										alt={"Picture of " + name}
+										objectFit="cover"
+										rounded={30}
+										style={{ aspectRatio: "1" }}
+										src={image?.url ?? "/staff/default.png"}
+									/>
+									<Heading size="lg" pt={4}>
+										{name}
+									</Heading>
+									<Text as="i">{title}</Text>
+									{taglineElement}
+								</VStack>
+								<Box flex={2}>
+									{biography.map((s) =>
+										React.cloneElement(parseText(s), {
+											key:
+												s.plain_text +
+												JSON.stringify(s.annotations),
+										})
+									)}
+								</Box>
+							</HStack>
+						</ModalBody>
+						<ModalFooter>
+							{/* <Button>Other action</Button> */}
+							<Button onClick={onClose}>Close</Button>
+						</ModalFooter>
+					</ModalContent>
+				</Modal>
 			) : null}
 		</Stack>
 	);

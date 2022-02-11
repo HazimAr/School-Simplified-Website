5fdbe70a889e15a73adb0f372de24d7ef1829eef
Gui Guy <jedandeganwang1999@gmail.com>
2022-02-10 19:19:30 -0600

    Scholarship page reformatting for fun

--- src/pages/scholarship.tsx
+++ src/pages/scholarship.tsx
@@ -1,7 +1,8 @@
 import { getScholarshipData } from "@api/notion";
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Heading, Table, Tbody, Td, Tr } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
+import NextChakraLink from "@components/nextChakra";
 import { ScholarshipProps } from "types";
 
 export default function Scholarship({
@@ -12,40 +13,30 @@ export default function Scholarship({
 	return (
 		<Container py={19}>
 			<ContainerInside>
-				<Heading>This page is temporary lol how did u find it</Heading>
-				{scholarshipData.map((entry, i: number) => {
-					return (
-						<Flex key={i} py={2}>
-							<Text width="100%" fontSize={10}>
-								{entry.title}
-							</Text>
-							<Text width="100%" fontSize={10}>
-								{entry.link}
-							</Text>
-							<Text width="100%" fontSize={10}>
-								{entry.value}
-							</Text>
-							<Text width="100%" fontSize={10}>
-								{entry.international_or_domestic}
-							</Text>
-							<Text width="100%" fontSize={10}>
-								{entry.state}
-							</Text>
-							<Text width="100%" fontSize={10}>
-								{entry.eligible_grades}
-							</Text>
-							<Text width="100%" fontSize={10}>
-								{entry.open_date}
-							</Text>
-							<Text width="100%" fontSize={10}>
-								{entry.closing_date}
-							</Text>
-							<Text width="100%" fontSize={10}>
-								{entry.notes}
-							</Text>
-						</Flex>
-					);
-				})}
+				<Heading>Scholarships</Heading>
+				<Table size="sm">
+					<Tbody>
+						{scholarshipData.map((entry) => {
+							return (
+								<Tr key={entry.link} maxW="100%" fontSize={10}>
+									<Td>
+										<NextChakraLink href={entry.link}>
+											{entry.title}
+										</NextChakraLink>
+									</Td>
+									<Td></Td>
+									<Td>{entry.value}</Td>
+									<Td>{entry.international_or_domestic}</Td>
+									<Td>{entry.state}</Td>
+									<Td>{entry.eligible_grades}</Td>
+									<Td>{entry.open_date}</Td>
+									<Td>{entry.closing_date}</Td>
+									<Td>{entry.notes}</Td>
+								</Tr>
+							);
+						})}
+					</Tbody>
+				</Table>
 			</ContainerInside>
 		</Container>
 	);
